c2e4d55809706d7122f84d269d3eab0e29e5a58b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-03 15:00:47 -0500

    Use fork instead of spawn

--- src/browsers.js
+++ src/browsers.js
@@ -1,48 +1,46 @@
 'use strict'
 
 const { randomInt } = require('crypto')
-const { spawn } = require('process')
+const { fork } = require('child_process')
+const job = require('./job')
 
-module.exports = job => {
-  job.browsers = {}
+job.browsers = {}
 
-  function start (relativeUrl) {
-    if (!relativeUrl.startsWith('/')) {
-      relativeUrl = '/' + relativeUrl
-    }
-    if (relativeUrl.includes('?')) {
-      relativeUrl += '&'
-    } else {
-      relativeUrl += '?'
-    }
-
-    const id = randomInt(0xFFFFFFFF)
-
-    let url = `http://localhost:${job.port}${relativeUrl}__id__=${id}`
-    if (job.keepAlive) {
-      url += '&__keepAlive__'
-    }
-    console.log(url)
-    const process = spawn(job.command, job.options.split(' ')
-      .map(param => param
-        .replace('$url', url)
-        .replace('$id', id)
-      ), { detached: true })
-    let done
-    const promise = new Promise(resolve => {
-      done = resolve
-    })
-    job.browsers[id] = { process, done }
-    promise.id = id
-    return promise
+function start (relativeUrl) {
+  if (!relativeUrl.startsWith('/')) {
+    relativeUrl = '/' + relativeUrl
+  }
+  if (relativeUrl.includes('?')) {
+    relativeUrl += '&'
+  } else {
+    relativeUrl += '?'
   }
 
-  function stop (id) {
-    const { process, done } = job.browsers[id]
-    delete job.browsers[id]
-    process.kill('SIGKILL')
-    done()
-  }
+  const id = randomInt(0xFFFFFFFF)
 
-  return { start, stop }
+  let url = `http://localhost:${job.port}${relativeUrl}__id__=${id}`
+  if (job.keepAlive) {
+    url += '&__keepAlive__'
+  }
+  console.log(url)
+  const parameters = { url, id }
+  const args = job.args.split(' ')
+    .map(arg => arg.replace(/\$(\w+)\b/, (match, name) => parameters[name] || match))
+  const childProcess = fork(job.browser, args, { stdio: 'inherit' })
+  let done
+  const promise = new Promise(resolve => {
+    done = resolve
+  })
+  job.browsers[id] = { childProcess, done }
+  promise.id = id
+  return promise
 }
+
+function stop (id) {
+  const { childProcess, done } = job.browsers[id]
+  delete job.browsers[id]
+  childProcess.send({ command: 'stop' })
+  done()
+}
+
+module.exports = { start, stop }
