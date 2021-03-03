a40d69799e7dc32b6855fc63c7ed07f765ec1431
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-02 23:03:00 -0500

    loads mappings and start server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// main
+'use strict'
+
+const { join } = require('path')
+const job = require('./src/job')
+const endpoints = require('./src/mappings/endpoints')(job)
+const ui5 = require('./src/mappings/ui5')(job)
+const { check, log, serve } = require('reserve')
+
+async function main () {
+  const configuration = await check({
+    port: job.port,
+    mappings: [
+      ...endpoints,
+      ...ui5, {
+      // Project mapping
+        match: /^\/(.*)/,
+        file: join(job.cwd, job.webapp, '$1')
+      }]
+  })
+  const server = serve(configuration)
+  if (job.logServer) {
+    log(server)
+  }
+  server
+    .on('ready', ({ port }) => {
+      job.port = port
+    })
+}
+
+main()
