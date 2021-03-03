9c2ed2e84452e4ac2cb37a4ba7528925f86d544e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-03 15:00:01 -0500

    All features

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 const { join } = require('path')
 const job = require('./src/job')
-const endpoints = require('./src/mappings/endpoints')(job)
-const ui5 = require('./src/mappings/ui5')(job)
+const endpoints = require('./src/endpoints')
+const { instrument, mappings: coverage } = require('./src/coverage')
+const ui5 = require('./src/ui5')
+const executeTests = require('./src/tests')
 const { check, log, serve } = require('reserve')
 
 async function main () {
+  await instrument()
   const configuration = await check({
     port: job.port,
     mappings: [
       ...endpoints,
-      ...ui5, {
-      // Project mapping
+      ...ui5,
+      ...coverage, {
+        // Project mapping
         match: /^\/(.*)/,
         file: join(job.cwd, job.webapp, '$1')
       }]
@@ -24,6 +28,7 @@ async function main () {
   server
     .on('ready', ({ port }) => {
       job.port = port
+      executeTests()
     })
 }
 
