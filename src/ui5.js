c79a7781ab74017e292eac4edd3cab186d6c5ae9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-04 11:48:29 -0500

    Secure cache generation

--- src/ui5.js
+++ src/ui5.js
@@ -10,32 +10,47 @@ const job = require('./job')
 
 const [, hostName, version] = /https?:\/\/([^/]*)\/.*(\d+\.\d+\.\d+)?$/.exec(job.ui5)
 const cacheBase = join(job.cwd, job.cache, hostName, version || '')
+const match = /\/((?:test-)?resources\/.*)/
 const ifCacheEnabled = (request, url, match) => job.cache ? match : false
+const cacheInProgress = {}
 
 module.exports = [{
+  // Avoid reading UI5 file while it's being written
+  match,
+  'if-match': ifCacheEnabled,
+  custom: async (request, response, path) => {
+    const cachingPromise = cacheInProgress[path]
+    if (cachingPromise) {
+      await cachingPromise
+    }
+  }
+}, {
   // UI5 from cache
-  match: /\/((?:test-)?resources\/.*)/,
+  match,
   'if-match': ifCacheEnabled,
   file: `${cacheBase}/$1`,
   'ignore-if-not-found': true
 }, {
   // UI5 caching
   method: 'GET',
-  match: /\/((?:test-)?resources\/.*)/,
+  match,
   'if-match': ifCacheEnabled,
   custom: async (request, response, path) => {
     const cachePath = join(cacheBase, path)
     const cacheFolder = dirname(cachePath)
     await mkdirAsync(cacheFolder, { recursive: true })
     const file = createWriteStream(cachePath)
-    capture(response, file)
+    cacheInProgress[path] = capture(response, file)
       .catch(reason => {
         console.error(`Unable to cache ${cachePath}`, reason)
+      })
+      .then(() => {
+        delete cacheInProgress[path]
       })
   }
 }, {
   // UI5 from url
   method: ['GET', 'HEAD'],
-  match: /\/((?:test-)?resources\/.*)/,
+  match,
   url: `${job.ui5}/$1`
 }]
