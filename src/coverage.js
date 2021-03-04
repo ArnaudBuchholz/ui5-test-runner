7129507c33602519e17ae2b0ee4912b14d8f2d1e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-04 10:28:22 -0500

    Fix coverage generation

--- src/coverage.js
+++ src/coverage.js
@@ -1,24 +1,27 @@
 'use strict'
 
 const { join } = require('path')
-const { spawn } = require('child_process')
+const { fork } = require('child_process')
 const { promisify } = require('util')
-const { readdir, readFile, rmdir, stat } = require('fs')
+const { mkdir, readdir, readFile, rmdir, stat, writeFile } = require('fs')
+const mkdirAsync = promisify(mkdir)
 const rmdirAsync = promisify(rmdir)
 const readdirAsync = promisify(readdir)
 const readFileAsync = promisify(readFile)
 const statAsync = promisify(stat)
+const writeFileAsync = promisify(writeFile)
 const { Readable } = require('stream')
 
 const job = require('./job')
 
 function nyc (...args) {
-  const process = spawn('node', [join(__dirname, '../node_modules/nyc/bin/nyc.js'), ...args], {
+  console.log('nyc', ...args)
+  const childProcess = fork(join(__dirname, '../node_modules/nyc/bin/nyc.js'), args, {
     stdio: 'inherit'
   })
   let done
   const promise = new Promise(resolve => { done = resolve })
-  process.on('close', done)
+  childProcess.on('close', done)
   return promise
 }
 
@@ -46,25 +49,21 @@ const tempDir = join(job.cwd, job.covTempDir)
 const instrumentedSourceDir = join(tempDir, job.webapp)
 const sourceDir = join(job.cwd, job.webapp)
 const reportDir = join(job.cwd, job.covReportDir)
+const settingsPath = join(tempDir, 'settings/nyc.json')
 
 async function instrument () {
-  console.log('Instrumenting...')
   await rmdirAsync(tempDir, { recursive: true })
-  await nyc('instrument', sourceDir, instrumentedSourceDir, '--nycrc-path', job.covSettings)
+  await mkdirAsync(join(tempDir, 'settings'), { recursive: true })
+  const settings = JSON.parse((await readFileAsync(job.covSettings)).toString())
+  settings.cwd = job.cwd
+  await writeFileAsync(settingsPath, JSON.stringify(settings))
+  await nyc('instrument', sourceDir, instrumentedSourceDir, '--nycrc-path', settingsPath)
 }
 
 async function generateCoverageReport () {
-  // if (job.coverage) {
-  //   const coverageFile = rel(`nyc/${results.id}.json`)
-  //   const coverageStat = await statAsync(coverageFile)
-  //   if (coverageStat) {
-  //     coverageFiles.push(coverageFile)
-  //   } else {
-  //     console.log('coverage file is missing')
-  //   }
-
+  await rmdirAsync(reportDir, { recursive: true })
   await nyc('merge', tempDir, join(tempDir, 'coverage.json'))
-  await nyc('report', '--reporter=lcov', '--temp-dir', tempDir, '--report-dir', reportDir)
+  await nyc('report', '--reporter=lcov', '--temp-dir', tempDir, '--report-dir', reportDir, '--nycrc-path', settingsPath)
 }
 
 const mappings = [{
