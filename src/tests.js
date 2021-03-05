3f9af8dda8d267c7bbed87ab0c1b87cf9884223b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-05 00:12:48 -0500

    Removes ID

--- src/tests.js
+++ src/tests.js
@@ -3,16 +3,14 @@
 const { start } = require('./browsers')
 const { generateCoverageReport } = require('./coverage')
 const job = require('./job')
-const progress = require('./progress')
 
 async function extractTestPages () {
-  progress('Extracting test pages')
-  await start('test/testsuite.qunit.html')
+  job.status = 'Extracting test pages'
+  await start('/test/testsuite.qunit.html')
   job.testPagesStarted = 0
   job.testPagesCompleted = 0
-  job.testPagesById = {}
-  job.testPagesByUrl = {}
-  progress('Executing test pages')
+  job.testPages = {}
+  job.status = 'Executing test pages'
   for (let i = 0; i < job.parallel; ++i) {
     runTestPage()
   }
@@ -28,34 +26,27 @@ async function runTestPage () {
   }
 
   const index = job.testPagesStarted++
-  const testPageUrl = job.testPageUrls[index]
-  const promise = start(testPageUrl)
-  const page = {
-    id: promise.id,
-    url: testPageUrl,
+  const url = job.testPageUrls[index]
+  const promise = start(url)
+  job.testPages[url] = {
     tests: [],
     wait: Promise.resolve()
   }
-  job.testPagesById[page.id] = page
-  job.testPagesByUrl[page.url] = page
-  progress()
 
   await promise
-  progress()
   ++job.testPagesCompleted
   runTestPage()
 }
 
 async function generateReport () {
-  progress('Finalizing')
-
+  job.status = 'Finalizing'
   // Simple report
   let failed = 0
-  for (const page of job.testPageUrls) {
-    console.log(page)
-    const results = job.testPagesByUrl[page]
-    if (results) {
-      console.table(results.tests.map(result => {
+  for (const url of job.testPageUrls) {
+    console.log(url)
+    const page = job.testPages[url]
+    if (page) {
+      console.table(page.tests.map(result => {
         return {
           name: result.name,
           passed: result.passed,
@@ -63,8 +54,8 @@ async function generateReport () {
           total: result.total
         }
       }))
-      console.log(results.report)
-      failed += results.report.failed
+      console.log(page.report)
+      failed += page.report.failed
     } else {
       console.log('(skipped)')
     }
