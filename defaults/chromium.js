bdc86989cf3757db1828cc48d3b0c3451bee5b4c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-07 23:00:29 -0500

    Extracts console

--- defaults/chromium.js
+++ defaults/chromium.js
@@ -1,8 +1,18 @@
 'use strict'
+
 const puppeteer = require('puppeteer')
+const { join } = require('path')
+const { createWriteStream } = require('fs')
+
 const url = process.argv[2]
+const reportDir = process.argv[3]
 
 let browser
+let consoleStream
+
+if (reportDir) {
+  consoleStream = createWriteStream(join(reportDir, 'console.txt'), 'utf-8')
+}
 
 process.on('message', async message => {
   if (message.command === 'stop') {
@@ -21,6 +31,10 @@ async function main () {
       '--disable-extensions'
     ]
   })
+  if (consoleStream) {
+    const page = (await browser.pages())[0]
+    page.on('console', message => consoleStream.write(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}\n`))
+  }
 }
 
 main()
