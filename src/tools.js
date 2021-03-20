04000ef1a0569f385479a99334fdf81861d41a3e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-19 23:45:53 -0400

    Removes promisify

--- src/tools.js
+++ src/tools.js
@@ -1,15 +1,12 @@
 'use strict'
 
-const { mkdir, rmdir } = require('fs')
-const { promisify } = require('util')
-const mkdirAsync = promisify(mkdir)
-const rmdirAsync = promisify(rmdir)
+const { mkdir, rmdir } = require('fs').promises
 
 const recursive = { recursive: true }
 
 module.exports = {
   filename: url => escape(url).replace(/\//g, '_'),
-  cleanDir: dir => rmdirAsync(dir, recursive),
-  createDir: dir => mkdirAsync(dir, recursive),
-  recreateDir: dir => rmdirAsync(dir, recursive).then(() => mkdirAsync(dir, recursive))
+  cleanDir: dir => rmdir(dir, recursive),
+  createDir: dir => mkdir(dir, recursive),
+  recreateDir: dir => rmdir(dir, recursive).then(() => mkdir(dir, recursive))
 }
