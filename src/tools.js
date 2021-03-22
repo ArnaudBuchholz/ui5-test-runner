e97e7949d9b6b5afeb49610c746ad00e5e678b63
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-03-21 23:40:35 -0400

    Ensures filename can be loaded via http

--- src/tools.js
+++ src/tools.js
@@ -5,7 +5,9 @@ const { mkdir, rmdir } = require('fs').promises
 const recursive = { recursive: true }
 
 module.exports = {
-  filename: url => escape(url).replace(/\//g, '_'),
+  filename: url => escape(url)
+    .replace(/\//g, '_')
+    .replace(/%([0-9a-z]{2})/ig, (match, hexa) => `_${hexa}_`),
   cleanDir: dir => rmdir(dir, recursive),
   createDir: dir => mkdir(dir, recursive),
   recreateDir: dir => rmdir(dir, recursive).then(() => mkdir(dir, recursive))
