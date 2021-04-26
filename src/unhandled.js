06ecfff7191d092fa1cd8498f46b4ad593c592be
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-25 23:51:54 -0400

    Log unhandled requests

---
+++ src/unhandled.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { extractUrl } = require('./tools')
+
+module.exports = [{
+  custom: ({ headers, method, url }, response) => {
+    if (method === 'GET' && url.match(/-preload\.js$|i18n_\w+\.properties$/)) {
+      return 404 // expected
+    }
+    if (method === 'GET') {
+      console.warn(`?? ${extractUrl(headers)} 404 ${method} ${url}`)
+      return 404
+    }
+    console.error(`!! ${extractUrl(headers)} 500 ${method} ${url}`)
+    return 500
+  }
+}]
