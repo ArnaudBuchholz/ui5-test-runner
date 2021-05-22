4a880eda8d8531a75de08be033cec621da0cbdcd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-22 10:15:02 -0400

    Fix child_process

--- __tests__/src/browser.test.js
+++ __tests__/src/browser.test.js
@@ -1,24 +1,20 @@
+jest.mock('child_process') 
+const { _hook: hook } = require('child_process')
 const { start, stop } = require('../../src/browsers')
 
 describe('src/job', () => {
   let log
-  let hook
 
   beforeEach(() => {
-    jest.mock('child_process') 
-    hook = require('child_process')._hook
     // log = jest.spyOn(console, 'log').mockImplementation()
   })
 
-  describe('normal flow', () => {
-    it('starts a command', () => {
-      hook.once('new', childProcess => {
-
-      })
-      const promise = start('test.html')
+  it('starts and stop a browser', () => {
+    hook.once('new', childProcess => {
+      console.log('stop ?')
       stop('test.html')
-      return promise
     })
+    return start('test.html')
   })
 
   afterAll(() => {
