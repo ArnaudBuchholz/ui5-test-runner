336b7ca96696eeb6b567f86983bc3e8273f14fbe
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-24 01:00:33 -0400

    wip

--- __tests__/src/browser.test.js
+++ __tests__/src/browser.test.js
@@ -1,20 +1,48 @@
-jest.mock('child_process') 
-const { _hook: hook } = require('child_process')
+jest.mock('child_process')
+const { _hook: hook } = require('child_process')
 const { start, stop } = require('../../src/browsers')
+
+const jobPath = '../../src/job.js'
+const cwdPath = '/test/project'
+const cwd = () => cwdPath
 
 describe('src/job', () => {
   let log
 
-  beforeEach(() => {
+  beforeAll(() => {
     log = jest.spyOn(console, 'log').mockImplementation()
   })
 
-  it('starts and stop a browser', () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('starts returns a promise resolved on stop', () => {
     hook.once('new', childProcess => {
-      console.log('stop ?')
       stop('test.html')
     })
     return start('test.html')
+  })
+
+  it('stops automatically after the page timeout', () => {
+    let done
+    const waitingForMessage = new Promise(resolve => {
+      done = resolve
+    })
+    hook.once('new', childProcess => {
+      childProcess.on('message', message => {
+        if (message.command === 'stop') {
+          done()
+        }
+      })
+    })
+    global.process = { cwd, argv: ['node', 'ui5-test-runner', '-pageTimeout:100'] }
+    const job = require(jobPath)
+    expect(job.pageTimeout).toStrictEqual(100)
+    return Promise.all([
+      start('test.html'),
+      waitingForMessage
+    ])
   })
 
   afterAll(() => {
