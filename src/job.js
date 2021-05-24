2e6bd965a47efdcb30f75cbb363414f19a9e0413
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-24 00:51:06 -0400

    Transformed into factory

--- src/job.js
+++ src/job.js
@@ -2,60 +2,76 @@
 
 const { join, isAbsolute } = require('path')
 
-const job = {
-  cwd: process.cwd(),
-  port: 0,
-  ui5: 'https://ui5.sap.com/1.87.0',
-  libs: '',
-  cache: '',
-  webapp: 'webapp',
-  pageFilter: '',
-  pageParams: '',
-  pageTimeout: 0,
-  globalTimeout: 0,
-  keepAlive: false,
-  watch: false,
-  logServer: false,
+function allocate (cwd) {
+  return {
+    cwd,
+    port: 0,
+    ui5: 'https://ui5.sap.com/1.87.0',
+    libs: '',
+    cache: '',
+    webapp: 'webapp',
+    pageFilter: '',
+    pageParams: '',
+    pageTimeout: 0,
+    globalTimeout: 0,
+    keepAlive: false,
+    watch: false,
+    logServer: false,
 
-  browser: join(__dirname, '../defaults/chromium.js'),
-  args: '__URL__ __REPORT__',
+    browser: join(__dirname, '../defaults/chromium.js'),
+    args: '__URL__ __REPORT__',
 
-  parallel: 2,
-  tstReportDir: 'report',
+    parallel: 2,
+    tstReportDir: 'report',
 
-  coverage: true,
-  covSettings: join(__dirname, '../defaults/nyc.json'),
-  covTempDir: '.nyc_output',
-  covReportDir: 'coverage',
-  covReporters: 'lcov,cobertura'
+    coverage: true,
+    covSettings: join(__dirname, '../defaults/nyc.json'),
+    covTempDir: '.nyc_output',
+    covReportDir: 'coverage',
+    covReporters: 'lcov,cobertura'
+  }
 }
 
-process.argv.forEach(arg => {
-  const valueParsers = {
-    boolean: value => value === 'true',
-    number: value => parseInt(value, 10),
-    default: value => value
-  }
-
-  const parsed = /-(\w+):(.*)/.exec(arg)
-  if (parsed) {
-    const [, name, value] = parsed
-    if (Object.prototype.hasOwnProperty.call(job, name)) {
-      const valueParser = valueParsers[typeof job[name]] || valueParsers.default
-      job[name] = valueParser(value)
+function finalize (job) {
+  function toAbsolute (member, from = job.cwd) {
+    if (!isAbsolute(job[member])) {
+      job[member] = join(from, job[member])
     }
   }
-})
 
-function toAbsolute (member, from = job.cwd) {
-  if (!isAbsolute(job[member])) {
-    job[member] = join(from, job[member])
-  }
+  toAbsolute('cwd', process.cwd())
+  'libs,webapp,browser,tstReportDir,covSettings,covTempDir,covReportDir'
+    .split(',')
+    .forEach(setting => toAbsolute(setting))
 }
 
-toAbsolute('cwd', process.cwd())
-'libs,webapp,browser,tstReportDir,covSettings,covTempDir,covReportDir'
-  .split(',')
-  .forEach(setting => toAbsolute(setting))
+module.exports = {
+  fromCmdLine (cwd, argv) {
+    const job = allocate(cwd)
+    argv.forEach(arg => {
+      const valueParsers = {
+        boolean: value => value === 'true',
+        number: value => parseInt(value, 10),
+        default: value => value
+      }
 
-module.exports = job
+      const parsed = /-(\w+):(.*)/.exec(arg)
+      if (parsed) {
+        const [, name, value] = parsed
+        if (Object.prototype.hasOwnProperty.call(job, name)) {
+          const valueParser = valueParsers[typeof job[name]] || valueParsers.default
+          job[name] = valueParser(value)
+        }
+      }
+    })
+    finalize(job)
+    return job
+  },
+
+  fromProperties (properties) {
+    const job = allocate()
+    Object.assign(job, properties)
+    finalize(job)
+    return job
+  }
+}
