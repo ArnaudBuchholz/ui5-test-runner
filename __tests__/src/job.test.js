ca4902dd83b1e2e58a4f5a7f988eaae809a30c90
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-24 01:00:27 -0400

    Adapt to job factory

--- __tests__/src/job.test.js
+++ __tests__/src/job.test.js
@@ -1,19 +1,13 @@
-const { dirname, join } = require('path')
+const { dirname } = require('path')
 
-const jobPath = '../../src/job.js'
-const cwdPath = '/test/project'
-const cwd = () => cwdPath
+const jobFactory = require('../../src/job')
+const cwd = '/test/project'
 const normalizePath = path => path.replace(/\\/g, '/') // win -> unix
 
 describe('src/job', () => {
-  beforeEach(() => {
-    jest.resetModules()
-  })
-
   it('provides default values', () => {
-    global.process = { cwd, argv: [] }
-    const job = require(jobPath)
-    expect(normalizePath(job.cwd)).toStrictEqual(cwdPath)
+    const job = jobFactory.fromCmdLine(cwd, [])
+    expect(normalizePath(job.cwd)).toStrictEqual(cwd)
     expect(job.port).toStrictEqual(0)
     expect(job.ui5).toStrictEqual('https://ui5.sap.com/1.87.0')
     expect(job.browser.startsWith(dirname(dirname(__dirname)))).toStrictEqual(true)
@@ -23,8 +17,7 @@ describe('src/job', () => {
   })
 
   it('parses parameters', () => {
-    global.process = { cwd, argv: ['node', 'ui5-test-runner', '-cwd:../project2', '-port:8080', '-keepAlive:true'] }
-    const job = require(jobPath)
+    const job = jobFactory.fromCmdLine(cwd, ['node', 'ui5-test-runner', '-cwd:../project2', '-port:8080', '-keepAlive:true'])
     expect(normalizePath(job.cwd)).toStrictEqual('/test/project2')
     expect(job.port).toStrictEqual(8080)
     expect(job.keepAlive).toStrictEqual(true)
@@ -32,12 +25,7 @@ describe('src/job', () => {
   })
 
   it('ignores unknown parameters', () => {
-    global.process = { cwd, argv: ['node', 'ui5-test-runner', '-cwd2:../project2'] }
-    const job = require(jobPath)
+    const job = jobFactory.fromCmdLine(cwd, ['node', 'ui5-test-runner', '-cwd2:../project2'])
     expect(normalizePath(job.cwd)).toStrictEqual('/test/project')
-  })
-
-  afterAll(() => {
-    delete global.process
   })
 })
