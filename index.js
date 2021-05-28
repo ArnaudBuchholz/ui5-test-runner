f8ecc099ba7b3e4d10d1634db285f3bcdae1e67d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-28 13:56:31 -0400

    Isolates configuration building

--- index.js
+++ index.js
@@ -2,32 +2,14 @@
 
 'use strict'
 
-const { join } = require('path')
-const endpoints = require('./src/endpoints')
-const { mappings: coverage } = require('./src/coverage')
-const ui5 = require('./src/ui5')
-const executeTests = require('./src/tests')
-const { check, log, serve } = require('reserve')
-const unhandled = require('./src/unhandled')
+const { log, serve } = require('reserve')
 const jobFactory = require('./src/job')
+const reserveConfigurationFactory = require('./src/reserve')
+const executeTests = require('./src/tests')
 
 async function main () {
   const job = jobFactory.fromCmdLine(process.cwd(), process.argv)
-  const configuration = await check({
-    port: job.port,
-    mappings: [
-      ...endpoints(job),
-      ...ui5(job),
-      ...coverage(job), {
-        // Project mapping
-        match: /^\/(.*)/,
-        file: join(job.webapp, '$1'),
-        strict: true,
-        'ignore-if-not-found': true
-      },
-      ...unhandled
-    ]
-  })
+  const configuration = await reserveConfigurationFactory(job)
   const server = serve(configuration)
   if (job.logServer) {
     log(server)
