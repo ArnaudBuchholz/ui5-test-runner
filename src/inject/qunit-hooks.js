feae00942ce5b298187324b6b8d86de76c8a9a57
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-19 07:39:34 -0400

    Detect OPA and wait for screenshot if needed (make it IE compatible)

--- src/inject/qunit-hooks.js
+++ src/inject/qunit-hooks.js
@@ -2,28 +2,59 @@
 (function () {
   'use strict'
 
-  function post (url, data, sync) {
-    const xhr = new XMLHttpRequest()
-    xhr.open('POST', '/_/' + url, !sync)
-    xhr.send(JSON.stringify(data))
+  function post (url, data) {
+    return new Promise(function (resolve, reject) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('POST', '/_/' + url)
+      xhr.send(JSON.stringify(data))
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText)
+          } else {
+            reject(xhr.statusText)
+          }
+        }
+      }
+    })
+  }
+
+  function isOpa () {
+    try {
+      return !!window.sap.ui.test.Opa5
+    } catch (e) {
+      return false
+    }
   }
 
   QUnit.begin(function (details) {
-    post('QUnit/begin', details)
+    details.isOpa = isOpa()
+    return post('QUnit/begin', details)
   })
 
   QUnit.log(function (report) {
-    post('QUnit/log', report, true)
+    var ready = false
+    post('QUnit/log', report).then(function () {
+      ready = true
+    })
+    if (isOpa()) {
+      window.sap.ui.test.Opa5.prototype.waitFor({
+        timeout: 10,
+        check: function () {
+          return ready
+        }
+      })
+    }
   })
 
   QUnit.testDone(function (report) {
-    post('QUnit/testDone', report, true)
+    return post('QUnit/testDone', report)
   })
 
   QUnit.done(function (report) {
     if (window.__coverage__) {
       report.__coverage__ = window.__coverage__
     }
-    post('QUnit/done', report)
+    return post('QUnit/done', report)
   })
 }())
