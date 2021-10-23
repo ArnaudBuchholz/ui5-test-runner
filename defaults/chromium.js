72aacd226dd9de5989266eb52ed9c8f0056ac090
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-22 23:43:13 -0400

    Secure message handling

--- defaults/chromium.js
+++ defaults/chromium.js
@@ -16,20 +16,25 @@ if (reportDir) {
 }
 
 process.on('message', async message => {
-  if (message.command === 'stop') {
-    await browser.close()
-    process.exit(0)
-  } else if (message.command === 'screenshot') {
-    if (reportDir && page) {
-      await page.screenshot({ path: join(reportDir, message.filename) })
-      process.send(message)
+  try {
+    if (message.command === 'stop') {
+      await browser.close()
+      process.exit(0)
+    } else if (message.command === 'screenshot') {
+      if (reportDir && page) {
+        await page.screenshot({ path: join(reportDir, message.filename) })
+        process.send(message)
+      }
+    } else if (message.command === 'capabilities') {
+      process.send({
+        command: 'capabilities',
+        screenshot: true,
+        consoleLog: true
+      })
     }
-  } else if (message.command === 'capabilities') {
-    process.send({
-      command: 'capabilities',
-      screenshot: true,
-      consoleLog: true
-    })
+  } catch (e) {
+    console.error(e)
+    process.exit(-2)
   }
 })
 
