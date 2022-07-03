b51921ae7bc5407bc5990eaab53d5d5e4771cf31
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-03 11:15:32 -0400

    Factorize post

--- src/inject/qunit-hooks.js
+++ src/inject/qunit-hooks.js
@@ -7,24 +7,7 @@
   }
   window['ui5-test-runner/qunit-hooks'] = true
 
-  const base = window['ui5-test-runner/base-host'] || ''
-
-  function post (url, data) {
-    return new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('POST', base + '/_/' + url)
-      xhr.send(JSON.stringify(data))
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(xhr.responseText)
-          } else {
-            reject(xhr.statusText)
-          }
-        }
-      }
-    })
-  }
+  const post = window['ui5-test-runner/post']
 
   function isOpa () {
     try {
