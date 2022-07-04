9e79419edd4f0b22829b1955d414eb4a1e245630
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-03 22:24:40 -0400

    Sequentialize requests

--- src/inject/post.js
+++ src/inject/post.js
@@ -3,21 +3,27 @@
 
   const base = window['ui5-test-runner/base-host'] || ''
 
+  let lastPost = Promise.resolve()
+
   window['ui5-test-runner/post'] = function post (url, data) {
-    return new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest()
-      xhr.open('POST', base + '/_/' + url)
-      xhr.setRequestHeader('x-page-url', location)
-      xhr.send(JSON.stringify(data))
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(xhr.responseText)
-          } else {
-            reject(xhr.statusText)
+    function request () {
+      return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest()
+        xhr.open('POST', base + '/_/' + url)
+        xhr.setRequestHeader('x-page-url', location)
+        xhr.send(JSON.stringify(data))
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+              resolve(xhr.responseText)
+            } else {
+              reject(xhr.statusText)
+            }
           }
         }
-      }
-    })
+      })
+    }
+    lastPost = lastPost.then(request)
+    return lastPost
   }
 }())
