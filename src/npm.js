ff5659e0ab9339f147df34f3f65542ef0426773a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-04 07:14:40 -0400

    Isolate package resolving

---
+++ src/npm.js
@@ -0,0 +1,49 @@
+const { exec } = require('child_process')
+const { join } = require('path')
+const { stat } = require('fs/promises')
+const output = require('./output')
+
+function npm (...args) {
+  return new Promise((resolve, reject) => {
+    const childProcess = exec(`npm ${args.join(' ')}`, (err, stdout, stderr) => {
+      if (err) {
+        reject(stderr)
+      } else {
+        resolve(stdout.trim())
+      }
+    })
+    if (args[0] === 'install') {
+      output.monitor(childProcess)
+    }
+  })
+}
+
+async function folderExists (path) {
+  try {
+    const result = await stat(path)
+    return result.isDirectory()
+  } catch (e) {
+    return false
+  }
+}
+
+let localRoot
+let globalRoot
+
+module.exports = async function (job, name) {
+  if (!localRoot) {
+    [localRoot, globalRoot] = await Promise.all([npm('root'), npm('root', '--global')])
+  }
+  const localModule = join(localRoot, name)
+  if (await folderExists(localModule)) {
+    return localModule
+  }
+  const globalModule = join(globalRoot, name)
+  if (!await folderExists(globalModule)) {
+    const previousStatus = job.status
+    job.status = `Installing ${name}...`
+    await npm('install', name, '-g')
+    job.status = previousStatus
+  }
+  return globalModule
+}
