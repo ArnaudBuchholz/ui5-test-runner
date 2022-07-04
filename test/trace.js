94414296842053ebab0e75ca3a0e57cbc8eb2075
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-04 18:04:08 -0400

    No more needed

--- test/trace.js
+++
@@ -1,7 +0,0 @@
-const { body } = require('reserve')
-
-module.exports = async (request, response) => {
-  console.log(request.headers.referer, JSON.parse(await body(request)))
-  response.writeHead(200)
-  response.end()
-}
