5de76dcb392bd80e1068d05ecb07a796e340de00
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-30 11:46:15 -0400

    Testing error normalization

---
+++ src/error.spec.js
@@ -0,0 +1,10 @@
+const { UTRError } = require('./error')
+describe('src/errors', () => {
+  it('generates a fully documented error', () => {
+    const error = UTRError.NPM_FAILED('test')
+    expect(error.name).toStrictEqual('UTRError')
+    expect(error.code).toBeLessThan(0)
+    expect(error.message).toStrictEqual('NPM_FAILED')
+    expect(error.details).toStrictEqual('test')
+  })
+})
