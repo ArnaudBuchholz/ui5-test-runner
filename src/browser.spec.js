ff6c98ca0c35231df68ec9396a605d34ed73bd72
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-30 16:23:40 -0400

    Use new child_process mocking

--- src/browser.spec.js
+++ src/browser.spec.js
@@ -1,10 +1,10 @@
 const output = require('./output')
 const { join } = require('path')
-const { _hook: hook } = require('child_process')
+const { reset, mock } = require('child_process')
 const jobFactory = require('./job')
 const { probe, start, stop, screenshot } = require('./browsers')
 const { readFile, writeFile } = require('fs/promises')
-const { createDir } = require('./tools')
+const { createDir, allocPromise } = require('./tools')
 
 const cwd = '/test/project'
 const tmp = join(__dirname, '../tmp')
@@ -13,122 +13,167 @@ describe('src/browser', () => {
   let job
 
   beforeAll(() => {
-    job = jobFactory.fromCmdLine(cwd, ['-url:about:blank', `-tstReportDir:${join(tmp, 'browser')}`])
+    job = jobFactory.fromCmdLine(cwd, [
+      '-url:about:blank',
+      `-tstReportDir:${join(tmp, 'browser')}`
+    ])
   })
 
-  afterEach(() => {
-    hook.removeAllListeners('new')
-  })
+  afterEach(reset)
 
   describe('probe', () => {
     it('starts the command with a specific config file', async () => {
-      hook.on('new', async childProcess => {
-        const config = JSON.parse((await readFile(childProcess.args[0])).toString())
-        expect(config.url).toStrictEqual('about:blank')
-        await writeFile(config.capabilities, '{}')
-        childProcess.close()
+      mock({
+        api: 'fork',
+        scriptPath: job.browser,
+        exec: async childProcess => {
+          const config = JSON.parse((await readFile(childProcess.args[0])).toString())
+          expect(config.url).toStrictEqual('about:blank')
+          await writeFile(config.capabilities, '{}')
+        }
       })
       await probe(job)
       expect(job.browserCapabilities.console).toStrictEqual(false)
     })
 
     it('fails if the browser does not generate capabilities', async () => {
-      hook.on('new', async childProcess => childProcess.close())
-      await expect(probe(job)).rejects
+      mock({
+        api: 'fork',
+        scriptPath: job.browser,
+        exec: () => {}
+      })
+      await expect(probe(job)).rejects.toMatchObject({
+        name: 'UTRError',
+        message: 'MISSING_OR_INVALID_BROWSER_CAPABILITIES'
+      })
     })
 
     it('reads and merge browser capabilities', async () => {
-      hook.on('new', async childProcess => {
-        const config = JSON.parse((await readFile(childProcess.args[0])).toString())
-        await writeFile(config.capabilities, JSON.stringify({
-          screenshot: false,
-          console: true
-        }))
-        childProcess.close()
+      mock({
+        api: 'fork',
+        scriptPath: job.browser,
+        exec: async childProcess => {
+          const config = JSON.parse((await readFile(childProcess.args[0])).toString())
+          expect(config.url).toStrictEqual('about:blank')
+          await writeFile(config.capabilities, JSON.stringify({
+            screenshot: false,
+            console: true
+          }))
+        }
       })
       await probe(job)
       expect(job.browserCapabilities.console).toStrictEqual(true)
       expect(job.browserCapabilities.parallel).toStrictEqual(true)
     })
 
-    it('handles dependent modules', async () => {
+    describe('dependent modules', () => {
       const npmLocal = join(tmp, 'npm/local')
       const npmGlobal = join(tmp, 'npm/global')
-      await createDir(join(npmLocal, 'localModule'))
-      await createDir(npmGlobal)
-      hook.on('new', async childProcess => {
-        if (childProcess.scriptPath === 'npm') {
-          if (childProcess.args[0] === 'root') {
-            if (childProcess.args[1] === '--global') {
-              childProcess.stdout.write(npmGlobal)
-            } else {
-              childProcess.stdout.write(npmLocal)
-            }
-          } else if (childProcess.args[0] === 'install') {
-            childProcess.stdout.write('OK')
-          }
-        } else {
-          const config = JSON.parse((await readFile(childProcess.args[0])).toString())
-          await writeFile(config.capabilities, JSON.stringify({
-            modules: ['localModule', 'globalModule']
-          }))
-        }
-        childProcess.close()
+
+      beforeEach(async () => {
+        await createDir(npmGlobal)
+        mock({
+          api: 'exec',
+          scriptPath: 'npm',
+          args: ['root', '--global'],
+          exec: childProcess => childProcess.stdout.write(npmGlobal)
+        })
+        await createDir(join(npmLocal, 'localModule'))
+        mock({
+          api: 'exec',
+          scriptPath: 'npm',
+          args: ['root'],
+          exec: childProcess => childProcess.stdout.write(npmLocal)
+        })
       })
-      await probe(job)
-      expect(job.browserCapabilities.modules.localModule).toStrictEqual(join(npmLocal, 'localModule'))
-      expect(job.browserCapabilities.modules.globalModule).toStrictEqual(join(npmGlobal, 'globalModule'))
+
+      it('handles dependent modules', async () => {
+        mock({
+          api: 'exec',
+          scriptPath: 'npm',
+          args: ['install', 'globalModule', '-g'],
+          exec: childProcess => childProcess.stdout.write('OK')
+        })
+        mock({
+          api: 'fork',
+          scriptPath: job.browser,
+          exec: async childProcess => {
+            const config = JSON.parse((await readFile(childProcess.args[0])).toString())
+            await writeFile(config.capabilities, JSON.stringify({
+              modules: ['localModule', 'globalModule']
+            }))
+          }
+        })
+        await probe(job)
+        expect(job.browserCapabilities.modules.localModule).toStrictEqual(join(npmLocal, 'localModule'))
+        expect(job.browserCapabilities.modules.globalModule).toStrictEqual(join(npmGlobal, 'globalModule'))
+      })
+
+      it('fails if a dependent module cannot be installed', async () => {
+        mock({
+          api: 'exec',
+          scriptPath: 'npm',
+          args: ['install', 'globalModule', '-g'],
+          exec: childProcess => {
+            childProcess.stdout.write('KO')
+            childProcess.close(-1)
+          },
+          close: false
+        })
+        mock({
+          api: 'fork',
+          scriptPath: job.browser,
+          exec: async childProcess => {
+            const config = JSON.parse((await readFile(childProcess.args[0])).toString())
+            await writeFile(config.capabilities, JSON.stringify({
+              modules: ['localModule', 'globalModule']
+            }))
+          }
+        })
+        await expect(probe(job)).rejects.toMatchObject({
+          name: 'UTRError',
+          message: 'NPM_FAILED'
+        })
+      })
+    })
+  })
+return
+  describe('start and stop', () => {
+    it('returns a promise resolved on stop', async () => {
+      hook.once('new', childProcess => {
+        setTimeout(() => stop(job, 'test.html'), 0)
+      })
+      await start(job, 'test.html')
+    })
+
+    it('stops automatically after a timeout', () => {
+      const { promise: waitingForStop, resolve: stopReceived } = allocPromise()
+      hook.once('new', childProcess => {
+        childProcess.on('message.received', message => {
+          if (message.command === 'stop') {
+            childProcess.close()
+            stopReceived()
+          }
+        })
+      })
+      job.pageTimeout = 100
+      return Promise.all([
+        start(job, 'test.html'),
+        waitingForStop
+      ])
+    })
+
+    it('ignores unknown pages', async () => {
+      await stop(job, 'unknown.html')
     })
   })
 
   return
 
-  it('starts returns a promise resolved on stop', () => {
-    hook.once('new', childProcess => {
-      setTimeout(() => stop(job, 'test.html'), 0)
-    })
-    return start(job, 'test.html')
+  describe('screenshot', () => {
+
   })
 
-  it('stops automatically after a timeout', () => {
-    let stopReceived
-    const waitingForMessage = new Promise(resolve => {
-      stopReceived = resolve
-    })
-    hook.once('new', childProcess => {
-      childProcess.on('message.received', message => {
-        if (message.command === 'stop') {
-          childProcess.close()
-          stopReceived()
-        }
-      })
-    })
-    job.pageTimeout = 100
-    return Promise.all([
-      start(job, 'test.html'),
-      waitingForMessage
-    ])
-  })
-
-  it('queries capabilities', () => {
-    output.once('browserCapabilities', capabilities => {
-      expect(capabilities.uid).toStrictEqual(123)
-      stop(job, 'test.html')
-    })
-    hook.once('new', childProcess => {
-      childProcess.on('message.received', message => {
-        if (message.command === 'capabilities') {
-          childProcess.emit('message', {
-            command: 'capabilities',
-            screenshot: true,
-            consoleLog: true,
-            uid: 123
-          })
-        }
-      })
-    })
-    return start(job, 'test.html')
-  })
 
   it('supports screenshot', () => {
     hook.once('new', childProcess => {
@@ -182,6 +227,10 @@ describe('src/browser', () => {
     return start(job, 'test.html')
   })
 
+  describe('automatic retry', () => {
+
+  })
+
   it('retries automatically if the process crashes unexpectedly (second succeeds)', () => {
     let step = 0
     hook.once('new', childProcess => {
@@ -214,9 +263,5 @@ describe('src/browser', () => {
       .then(() => {
         expect(step).toStrictEqual(2)
       })
-  })
-
-  it('ignores unstarted pages', () => {
-    stop(job, 'unknown.html')
   })
 })
