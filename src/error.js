882b2e04cd890eef98bf2cac103a569d9698b82a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-07-30 11:46:02 -0400

    Normalizing errors

---
+++ src/error.js
@@ -0,0 +1,29 @@
+class UTRError extends Error {
+  get code () { return this._code }
+  get details () { return this._details }
+
+  constructor (error, details) {
+    super()
+    this.name = 'UTRError'
+    this._code = error.code
+    this.message = error.name
+    this._details = details
+  }
+}
+
+const errors = [{
+  name: 'GENERIC'
+}, {
+  name: 'NPM_FAILED'
+}]
+
+errors.forEach((error, index) => {
+  error.code = -1 - index
+  UTRError[error.name] = function (details = '') {
+    return new UTRError(error, details)
+  }
+})
+
+module.exports = {
+  UTRError
+}
