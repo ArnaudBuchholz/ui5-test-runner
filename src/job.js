590e5ea817ca7fc12b4627769f1bc39852e37967
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-08-10 23:01:56 -0400

    integrating commander

--- src/job.js
+++ src/job.js
@@ -1,56 +1,10 @@
 'use strict'
 
-const { Command } = require('commander')
+const { Command, Option } = require('commander')
 const { accessSync } = require('fs')
 const { join, isAbsolute } = require('path')
 const output = require('./output')
 const { name, description, version } = require(join(__dirname, '../package.json'))
-
-function allocate (cwd) {
-  const job = new Command()
-  job
-    .name(name)
-    .description(description)
-    .version(version)
-    .option('-cwd <path>', 'Current working directory', cwd)
-    .option('-p, -port <port>', 'Port to use (0 to use a free one)', 0)
-    .option('-ui5 <url>', 'UI5 url', 'https://ui5.sap.com')
-
-  return {
-    initialCwd: cwd,
-    cwd,
-    port: 0,
-    ui5: 'https://ui5.sap.com',
-    libs: [],
-    cache: '',
-    webapp: 'webapp',
-    testsuite: 'test/testsuite.qunit.html',
-    url: '',
-    pageFilter: '',
-    pageParams: '',
-    pageTimeout: 0,
-    globalTimeout: 0,
-    failFast: false,
-    keepAlive: false,
-    watch: false,
-    logServer: false,
-
-    browser: join(__dirname, '../defaults/puppeteer.js'),
-    browserRetry: 1,
-    noScreenshot: false,
-    screenshotTimeout: 2000,
-    browserArgs: [],
-
-    parallel: 2,
-    tstReportDir: 'report',
-
-    coverage: true,
-    covSettings: join(__dirname, '../defaults/nyc.json'),
-    covTempDir: '.nyc_output',
-    covReportDir: 'coverage',
-    covReporters: 'lcov,cobertura'
-  }
-}
 
 function checkAccess (path, label) {
   try {
@@ -87,16 +41,20 @@ function finalize (job) {
   }
   checkAccess(job.browser, 'browser command')
 
-  job.libs.forEach(libMapping => {
-    libMapping.source = toAbsolute(libMapping.source)
-    let description
-    if (libMapping.relative) {
-      description = `lib mapping of ${libMapping.relative}`
-    } else {
-      description = 'generic lib mapping'
-    }
-    checkAccess(libMapping.source, `${description} (${libMapping.source})`)
-  })
+  if (!job.libs) {
+    job.libs = []
+  } else {
+    job.libs.forEach(libMapping => {
+      libMapping.source = toAbsolute(libMapping.source)
+      let description
+      if (libMapping.relative) {
+        description = `lib mapping of ${libMapping.relative}`
+      } else {
+        description = 'generic lib mapping'
+      }
+      checkAccess(libMapping.source, `${description} (${libMapping.source})`)
+    })
+  }
 
   if (job.parallel <= 0) {
     job.keepAlive = true
@@ -150,16 +108,74 @@ function parseJobParam (job, arg) {
   }
 }
 
+function lowerCaseKeys (dictionary) {
+  return Object.keys(dictionary).reduce((result, name) => {
+    const value = dictionary[name]
+    if (name.startsWith('!')) {
+      result[name] = value
+    } else {
+      result[name.charAt(0).toLocaleLowerCase() + name.substring(1)] = value
+    }
+    return result
+  }, {})
+}
+
 module.exports = {
   fromCmdLine (cwd, argv) {
-    const job = allocate(cwd)
+    const command = new Command()
+    command
+      .name(name)
+      .description(description)
+      .version(version)
+      .addOption(
+        new Option('-cwd <path>', 'Current working directory')
+          .default(cwd, 'current working directory')
+      )
+      .option('-port <port>', 'Port to use (0 to use a free one)', 0)
+      .option('-ui5 <url>', 'UI5 url', 'https://ui5.sap.com')
+      .option('-lib <path...>', 'Library mapping')
+      .option('-cache <path...>', 'Cache UI5 resources locally in the given folder (empty to disable)')
+      .option('-webapp <path...>', 'Base folder of the web application (relative to cwd)', 'webapp')
+      .option('-testsuite <path...>', 'Path of the testsuite file (relative to webapp)', 'test/testsuite.qunit.html')
+      .option('-url <url...>', 'URL of the testsuite / page to test')
+
+      .option('-pageFilter <regexp>', 'Filters which pages to execute')
+      .option('-pageParams <params>', 'Parameters added to each page URL')
+      .option('-pageTimeout <timeout>', 'Limit the page execution time (ms), fails the page if it takes longer than the timeout (0 to disable the timeout)', 0)
+      .option('-globalTimeout <timeout>', 'Limit the pages execution time (ms), fails the page if it takes longer than the timeout (0 to disable the timeout)', 0)
+      .option('-failFast', 'Stops the execution after the first failing page', false)
+      .option('-keepAlive', 'Keeps the server alive (enables debugging)', false)
+      .option('-watch', 'Monitors the webapp folder and re-execute tests on change', false)
+      .option('-logServer', 'Logs server traces', false)
+
+      .option('-browser <command>', 'Browser instantiation command', join(__dirname, '../defaults/puppeteer.js'))
+
+      .option('-browserRetry <count>', 'Browser instantiation retries : if the command fails unexpectedly, it is re-executed (0 means no retry)', 1)
+      .option('-noScreenshot', 'No screenshot is taken during the tests execution', false)
+      .option('-screenshotTimeout <timeout>', 'Maximum waiting time (ms) for browser screenshot', 2000)
+
+      .option('-parallel <count>', 'Number of parallel tests executions (0 to ignore tests and keep alive)', 2)
+      .option('-tstReportDir <path>', 'Directory to output test reports (relative to cwd)', 'report')
+
+      .option('-coverage <flag>', 'Enable or disable code coverage', true)
+      .option('-covSettings <path>', 'Path to a custom nyc.json file providing settings for instrumentation (relative to cwd)', join(__dirname, '../defaults/nyc.json'))
+      .option('-covTempDir <path>', 'Directory to output raw coverage information to (relative to cwd)', '.nyc_output')
+      .option('-covReportDir <path>', 'Directory to store the coverage report files (relative to cwd)', 'coverage')
+      .option('-covReporters <reporter...>', 'List of reporters to use', ['lcov', 'cobertura'])
+
+    let defaults
     try {
-      const defaults = require(join(cwd, 'ui5-test-runner.json'))
-      Object.assign(job, defaults)
+      defaults = require(join(cwd, 'ui5-test-runner.json'))
     } catch (e) {
-      // ignore
+      defaults = {}
     }
-    let defaultHasLibs = job.libs.length !== 0
+    command.parse(argv, { from: 'user' })
+    const job = Object.assign({
+      initialCwd: cwd
+    }, defaults, lowerCaseKeys(command.opts()))
+
+/*
+    let defaultHasLibs = defaults.libs.length !== 0
     let inBrowserParams = false
     argv.forEach(arg => {
       if (inBrowserParams) {
@@ -174,6 +190,8 @@ module.exports = {
         parseJobParam(job, arg)
       }
     })
+*/
+
     finalize(job)
     return job
   }
