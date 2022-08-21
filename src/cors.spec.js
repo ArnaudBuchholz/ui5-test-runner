7c23d3d3c158157bb19d139124518496cd7ecd8b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-08-21 01:06:00 -0400

    Improves coverage

--- src/cors.spec.js
+++ src/cors.spec.js
@@ -1,6 +1,5 @@
 'use script'
 
-const assert = require('assert')
 const Request = require('reserve/mock/Request')
 const Response = require('reserve/mock/Response')
 const cors = require('./cors')
@@ -12,19 +11,31 @@ describe('src/cors', () => {
     const request = new Request('GET', '/', { origin })
     const response = new Response()
     cors.custom(request, response)
-    assert.strictEqual(response.statusCode, undefined)
-    assert.strictEqual(response.headers['Access-Control-Allow-Origin'], origin)
-    assert.strictEqual(response.headers['Access-Control-Allow-Credentials'], 'true')
+    expect(response.statusCode).toBeUndefined()
+    expect(response.headers).toMatchObject({
+      'access-control-allow-origin': origin,
+      'access-control-allow-credentials': 'true'
+    })
+  })
+
+  it('does not impact response if origin is missing', async () => {
+    const request = new Request('GET', '/')
+    const response = new Response()
+    cors.custom(request, response)
+    expect(response.statusCode).toBeUndefined()
+    expect(response.headers).toMatchObject({})
   })
 
   it('handles CORS preflight', async () => {
     const request = new Request('OPTIONS', '/', { origin })
     const response = new Response()
     cors.custom(request, response)
-    assert.strictEqual(response.statusCode, 200)
-    assert.strictEqual(response.headers['Access-Control-Allow-Origin'], origin)
-    assert.strictEqual(response.headers['Access-Control-Allow-Headers'], 'content-type, content-length, x-page-url')
-    assert.strictEqual(response.headers['Access-Control-Allow-Methods'], 'GET, POST, PUT, DELETE, OPTIONS')
-    assert.strictEqual(response.headers['Access-Control-Allow-Credentials'], 'true')
+    expect(response.statusCode).toStrictEqual(200)
+    expect(response.headers).toMatchObject({
+      'access-control-allow-origin': origin,
+      'access-control-allow-headers': 'content-type, content-length, x-page-url',
+      'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'access-control-allow-credentials': 'true'
+    })
   })
 })
