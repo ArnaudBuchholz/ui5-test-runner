766f63e7830549ca52abf52450eb22d20c9cbb9c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-09-04 00:52:51 -0400

    Shorter (and unique) filenames

--- src/tools.js
+++ src/tools.js
@@ -2,6 +2,7 @@
 
 const fsPromises = require('fs').promises
 const { mkdir, stat } = fsPromises
+const { createHash } = require('crypto')
 
 let rm
 /* istanbul ignore next */ // Hard to test both in the same run
@@ -14,17 +15,11 @@ if (process.version > 'v14.14') {
 const recursive = { recursive: true }
 
 const filename = url => {
-  const [, protocol, host, rawPort, relativeUrl] = url.match(/(?:(https?):\/\/([^:/]+)(?::(\d+))?)?\/(.*)/)
-  const port = rawPort || protocol === 'https' ? '443' : '80'
-  let base
-  if (host) {
-    base = host + '_' + port + '_' + relativeUrl
-  } else {
-    base = relativeUrl
-  }
-  return encodeURIComponent(base)
+  const hash = createHash('sha256')
+  hash.update(url)
+  return hash.digest('base64')
+    .replace(/=/g, '')
     .replace(/\//g, '_')
-    .replace(/%([0-9a-z]{2})/ig, (match, hexa) => `_${hexa}_`)
 }
 
 const cleanDir = async dir => {
