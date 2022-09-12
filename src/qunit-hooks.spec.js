dcd0968048b88002ecf6caad7004fc7e207bbd77
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-09-12 07:10:14 -0400

    wip

--- src/qunit-hooks.spec.js
+++ src/qunit-hooks.spec.js
@@ -1,15 +1,22 @@
 jest.mock('./browsers.js', () => ({
-  screenshot: jest.fn()
+  screenshot: jest.fn(),
+  stop: jest.fn()
 }))
-const { screenshot } = require('./browsers')
+const { screenshot, stop } = require('./browsers')
 
 const {
   begin,
-  log
+  log,
+  testDone,
+  done
 } = require('./qunit-hooks.js')
 
 describe('src/qunit-hooks', () => {
   const url = 'http://localhost:80/page1.html'
+
+  beforeEach(() => {
+    screenshot.mockClear()
+  })
 
   describe('begin', () => {
     it('allocates qunitPages', async () => {
@@ -54,6 +61,46 @@ describe('src/qunit-hooks', () => {
         }]
       })
     })
+
+    it('resets the existing structure (retry)', async () => {
+      const job = {
+        qunitPages: {
+          [url]: {
+            isOpa: false,
+            failed: 2,
+            passed: 5,
+            tests: [{
+              id: '1a',
+              before: true
+            }, {
+              id: '2b',
+              before: true
+            }]
+          }
+        }
+      }
+      await begin(job, url, {
+        isOpa: false,
+        totalTests: 2,
+        modules: [{
+          tests: [{
+            testId: '1a'
+          }, {
+            testId: '2b'
+          }]
+        }]
+      })
+      expect(job.qunitPages[url]).toEqual({
+        isOpa: false,
+        failed: 0,
+        passed: 0,
+        tests: [{
+          id: '1a'
+        }, {
+          id: '2b'
+        }]
+      })
+    })
   })
 
   describe('log', () => {
@@ -64,6 +111,7 @@ describe('src/qunit-hooks', () => {
         modules: []
       })
       await log(job, url, {})
+      expect(screenshot).not.toHaveBeenCalled()
     })
 
     it('does nothing when screenshot is not available', async () => {
@@ -77,6 +125,7 @@ describe('src/qunit-hooks', () => {
         modules: []
       })
       await log(job, url, {})
+      expect(screenshot).not.toHaveBeenCalled()
     })
 
     it('triggers a screenshot for OPA tests', async () => {
@@ -109,5 +158,107 @@ describe('src/qunit-hooks', () => {
         }]
       })
     })
+  })
+
+  describe('testDone', () => {
+    it('increases passed count when successful', async () => {
+      const job = {}
+      await begin(job, url, {
+        isOpa: false,
+        modules: [{
+          tests: [{
+            testId: '1a'
+          }, {
+            testId: '2b'
+          }]
+        }]
+      })
+      const report = { testId: '2b', failed: false }
+      await testDone(job, url, report)
+      expect(screenshot).not.toHaveBeenCalled()
+      expect(job.qunitPages[url]).toEqual({
+        isOpa: false,
+        failed: 0,
+        passed: 1,
+        tests: [{
+          id: '1a'
+        }, {
+          id: '2b',
+          report
+        }]
+      })
+    })
+
+    describe('failure', () => {
+      it('increases failed count and takes a screenshot', async () => {
+        const job = {
+          browserCapabilities: {
+            screenshot: '.png'
+          }
+        }
+        await begin(job, url, {
+          isOpa: false,
+          modules: [{
+            tests: [{
+              testId: '1a'
+            }, {
+              testId: '2b'
+            }]
+          }]
+        })
+        const report = { testId: '2b', failed: true }
+        await testDone(job, url, report)
+        expect(screenshot).toHaveBeenCalledWith(job, url, '2b')
+        expect(job.qunitPages[url]).toEqual({
+          isOpa: false,
+          failed: 1,
+          passed: 0,
+          tests: [{
+            id: '1a'
+          }, {
+            id: '2b',
+            report
+          }]
+        })
+        expect(job.failed).toStrictEqual(true)
+      })
+
+      it('increases failed count', async () => {
+        const job = {
+          browserCapabilities: {}
+        }
+        await begin(job, url, {
+          isOpa: false,
+          modules: [{
+            tests: [{
+              testId: '1a'
+            }, {
+              testId: '2b'
+            }]
+          }]
+        })
+        const report = { testId: '2b', failed: true }
+        await testDone(job, url, report)
+        expect(screenshot).not.toHaveBeenCalled()
+        expect(job.qunitPages[url]).toEqual({
+          isOpa: false,
+          failed: 1,
+          passed: 0,
+          tests: [{
+            id: '1a'
+          }, {
+            id: '2b',
+            report
+          }]
+        })
+        expect(job.failed).toStrictEqual(true)
+      })
+    })
+  })
+
+  describe('done', () => {
+    const job = {
+
+    }
   })
 })
