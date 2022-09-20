029c1247c68aeba77e6b95bee1c906196a4cdf11
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-09-20 06:10:11 -0400

    Offers @/ shortcut for defaults impl

--- src/job.js
+++ src/job.js
@@ -106,7 +106,7 @@ function parse (cwd, args) {
     .option('-w, --watch [flag]', 'Monitors the webapp folder and re-execute tests on change', boolean, false)
     .option('-l, --log-server [flag]', 'Logs server traces', boolean, false)
 
-    .option('-b, --browser <command>', 'Browser instantiation command (relative to cwd)', join(__dirname, '../defaults/puppeteer.js'))
+    .option('-b, --browser <command>', 'Browser instantiation command (relative to cwd or use @/ for provided ones)', '@/puppeteer.js')
     .option('--browser-args <argument...>', 'Browser instantiation command parameters')
 
     .option('-bt, --browser-close-timeout <timeout>', 'Maximum waiting time (ms) for browser close', integer, 2000)
@@ -118,12 +118,12 @@ function parse (cwd, args) {
     .option('-r, --report-dir <path>', 'Directory to output test reports (relative to cwd)', 'report')
 
     .option('--coverage [flag]', 'Enable or disable code coverage', boolean, true)
-    .option('-cs, --coverage-settings <path>', 'Path to a custom nyc.json file providing settings for instrumentation (relative to cwd)', join(__dirname, '../defaults/nyc.json'))
+    .option('-cs, --coverage-settings <path>', 'Path to a custom nyc.json file providing settings for instrumentation (relative to cwd or use @/ for provided ones)', '@/nyc.json')
     .option('-ct, --coverage-temp-dir <path>', 'Directory to output raw coverage information to (relative to cwd)', '.nyc_output')
     .option('-cr, --coverage-report-dir <path>', 'Directory to store the coverage report files (relative to cwd)', 'coverage')
     .option('-cr, --coverage-reporters <reporter...>', 'List of reporters to use', ['lcov', 'cobertura'])
 
-    .option('-rg, --report-generator <path...>', 'Path to a report generator (relative to cwd)', [join(__dirname, '../defaults/report.js')])
+    .option('-rg, --report-generator <path...>', 'Path to a report generator (relative to cwd or use @/ for provided ones)', ['@/report.js'])
 
   command.parse(args, { from: 'user' })
   const options = command.opts()
@@ -166,10 +166,19 @@ function finalize (job) {
     return path
   }
 
+  function checkDefault (path) {
+    if (path.startsWith('@/')) {
+      return join(__dirname, '../defaults', path.replace('@/', ''))
+    }
+    return path
+  }
+
   function updateToAbsolute (member, from = job.cwd) {
     job[member] = toAbsolute(job[member], from)
   }
-
+  'browser,coverageSettings'
+    .split(',')
+    .forEach(setting => { job[setting] = checkDefault(job[setting]) })
   updateToAbsolute('cwd', job.initialCwd)
   'webapp,browser,reportDir,coverageSettings,coverageTempDir,coverageReportDir'
     .split(',')
@@ -181,7 +190,7 @@ function finalize (job) {
   }
   checkAccess({ path: job.browser, label: 'browser command', file: true })
   job.reportGenerator = job.reportGenerator.map(setting => {
-    const path = toAbsolute(setting, job.cwd)
+    const path = toAbsolute(checkDefault(setting), job.cwd)
     checkAccess({ path, label: 'report generator', file: true })
     return path
   })
