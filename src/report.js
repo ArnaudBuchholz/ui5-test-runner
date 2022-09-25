7885a764f77fdcdee825c7c3295709ffa1b9047a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-09-25 12:15:36 -0400

    Refactor output

--- src/report.js
+++ src/report.js
@@ -5,7 +5,7 @@ const { writeFile } = require('fs').promises
 const { generateCoverageReport } = require('./coverage')
 const { filename, allocPromise } = require('./tools')
 const { fork } = require('child_process')
-const output = require('./output')
+const { getOutput } = require('./output')
 
 async function serialize (job, filename, json) {
   await writeFile(join(job.reportDir, `${filename}.js`), `module.exports = ${JSON.stringify(json, undefined, 2)}`)
@@ -19,27 +19,11 @@ module.exports = {
   save,
 
   async generate (job) {
+    const output = getOutput(job)
     job.end = new Date()
+    job.failed = !!job.failed
     job.status = 'Generating report'
-    job.failed = 0
-    const pages = []
-    job.testPageUrls.forEach(url => {
-      const page = job.qunitPages[url]
-      if (page && page.report) {
-        pages.push({
-          url,
-          failed: page.report.failed
-        })
-        job.failed += page.report.failed
-      } else {
-        pages.push({
-          url,
-          failed: -1
-        })
-        job.failed += 1
-      }
-    })
-    output.results(pages)
+    output.results()
     job.testPageHashes = job.testPageUrls.map(url => filename(url))
     await save(job)
     const promises = job.reportGenerator.map(generator => {
