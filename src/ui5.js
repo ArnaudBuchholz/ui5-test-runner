77a17aba2e23ba0b1585133b0bade1644730e2e5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-09-25 12:15:59 -0400

    Refactor output

--- src/ui5.js
+++ src/ui5.js
@@ -4,7 +4,7 @@ const { dirname, join } = require('path')
 const { createWriteStream } = require('fs')
 const { mkdir, unlink } = require('fs').promises
 const { capture } = require('reserve')
-const output = require('./output')
+const { getOutput } = require('./output')
 
 module.exports = job => {
   const [, hostName] = /https?:\/\/([^/]*)/.exec(job.ui5)
@@ -59,7 +59,7 @@ module.exports = job => {
           file.end()
           uncachable[path] = true
           if (response.statusCode !== 404) {
-            output.failedToCacheUI5resource(path, response.statusCode)
+            getOutput(job).failedToCacheUI5resource(path, response.statusCode)
           }
           return unlink(cachePath)
         })
