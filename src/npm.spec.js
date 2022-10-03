a517aeabb3fae5b71ac70514c2cc6054374d0297
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-10-03 06:34:59 -0400

    Fixing NPM tests

--- src/npm.spec.js
+++ src/npm.spec.js
@@ -1,44 +1,47 @@
 const { join } = require('path')
 const { resolvePackage } = require('./npm')
-const { reset, mock } = require('child_process')
-const { createDir } = require('./tools')
+const { mock } = require('child_process')
+const { createDir, recreateDir } = require('./tools')
+const { fromObject } = require('./job')
+const { getOutput } = require('./output')
 
 const tmp = join(__dirname, '../tmp')
 const npmGlobal = join(tmp, 'npm/global')
 
 describe('src/npm', () => {
-  afterEach(() => reset())
+  const cwd = join(__dirname, '../test/project')
+  const reportDir = join(__dirname, '../tmp/npm/report')
+
+  let job
+  let output
 
   beforeAll(async () => {
     await createDir(join(npmGlobal, 'existing_global'))
+    await recreateDir(reportDir)
+  })
+
+  beforeEach(() => {
+    job = fromObject(cwd, {
+      reportDir,
+      coverage: false
+    })
+    job.status = 'Testing'
+    output = getOutput(job)
+    const statusImpl = output.status
+    jest.spyOn(output, 'status').mockImplementation(statusImpl)
   })
 
   it('detects already installed local package', async () => {
-    const path = await resolvePackage({}, 'reserve')
+    const path = await resolvePackage(job, 'reserve')
     expect(path).toStrictEqual(join(__dirname, '../node_modules/reserve'))
   })
 
   it('detects already installed global package', async () => {
-    const path = await resolvePackage({}, 'existing_global')
+    const path = await resolvePackage(job, 'existing_global')
     expect(path).toStrictEqual(join(npmGlobal, 'existing_global'))
   })
 
   it('installs missing package globally', async () => {
-    const job = {}
-    const $initial = Symbol('status')
-    let modified = false
-    let restored = false
-    Object.defineProperty(job, 'status', {
-      get: () => $initial,
-      set: value => {
-        if (value === $initial) {
-          restored = true
-        } else {
-          modified = true
-          restored = false
-        }
-      }
-    })
     mock({
       api: 'exec',
       scriptPath: 'npm',
@@ -46,9 +49,8 @@ describe('src/npm', () => {
       exec: childProcess => childProcess.stdout.write('OK installed')
     })
     const path = await resolvePackage(job, 'not_existing')
-    expect(modified).toStrictEqual(true)
-    expect(restored).toStrictEqual(true)
     expect(path).toStrictEqual(join(npmGlobal, 'not_existing'))
+    expect(output.status).toHaveBeenCalledTimes(2)
   })
 
   it('fails if the package cannot be installed', async () => {
@@ -58,9 +60,10 @@ describe('src/npm', () => {
       args: ['install', 'not_existing', '-g'],
       exec: childProcess => { throw new Error('KO failed') }
     })
-    await expect(resolvePackage({}, 'not_existing')).rejects.toMatchObject({
+    await expect(resolvePackage(job, 'not_existing')).rejects.toMatchObject({
       name: 'UTRError',
       message: 'NPM_FAILED'
     })
+    expect(output.status).toHaveBeenCalledTimes(1) // Won't restore previous status
   })
 })
