0c643b41637113cf6bf57962ae7b2c5b9c45ad7d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-10-02 23:11:54 -0400

    Adjusts coverage tests

--- src/coverage.spec.js
+++ src/coverage.spec.js
@@ -1,56 +1,42 @@
 const { join } = require('path')
-const jobFactory = require('../../src/job')
+const { fromObject } = require('./job')
 const { _hook: hook } = require('child_process')
-const { instrument, generateCoverageReport, mappings } = require('../../src/coverage')
-const { stat: statAsync } = require('fs').promises
-const { cleanDir } = require('../../src/tools')
-
-const cwd = '/test/project'
+const { instrument, generateCoverageReport, mappings } = require('./coverage')
+const { stat } = require('fs/promises')
+const { cleanDir, createDir } = require('./tools')
 
 describe('src/coverage', () => {
-  let log
-  let warn
-  let error
-  const coveragePath = join(__dirname, '../../tmp/coverage')
+  const cwd = join(__dirname, '../test/project')
+  const path = join(__dirname, '../tmp/coverage')
 
   beforeAll(() => {
-    log = jest.spyOn(console, 'log').mockImplementation()
-    warn = jest.spyOn(console, 'warn').mockImplementation()
-    error = jest.spyOn(console, 'error').mockImplementation()
-    return cleanDir(coveragePath)
+    return cleanDir(path)
   })
 
   describe('disabled', () => {
-    const path = join(coveragePath, 'disabled')
+    const basePath = join(path, 'disabled')
     let job
 
-    beforeAll(() => {
-      job = jobFactory.fromCmdLine(cwd, [0, 0,
-        `-covTempDir:${join(path, 'temp')}`,
-        `-covReportDir:${join(path, 'report')}`,
-        '-coverage:false'
-      ])
+    beforeAll(async () => {
+      const reportDir = join(basePath, 'report')
+      await createDir(reportDir)
+      job = fromObject(cwd, {
+        reportDir,
+        coverageTempDir: join(basePath, 'coverage/temp'),
+        coverageReportDir: join(basePath, 'coverage/report'),
+        coverage: false
+      })
     })
 
     it('does not instrument sources', async () => {
-      let triggered = false
-      hook.once('new', childProcess => {
-        triggered = true
-      })
       await instrument(job)
-      expect(triggered).toStrictEqual(false)
-      expect(() => statAsync(join(path, 'temp/settings/nyc.json'))).rejects.toThrow()
+      expect(() => stat(join(basePath, 'coverage/temp/settings/nyc.json'))).rejects.toThrow()
     })
 
     it('does not generate a report', async () => {
-      let triggered = false
-      hook.once('new', childProcess => {
-        triggered = true
-      })
       await generateCoverageReport(job)
-      expect(triggered).toStrictEqual(false)
-      expect(() => statAsync(join(path, 'temp/coverage.json'))).rejects.toThrow()
-      expect(() => statAsync(join(path, 'report'))).rejects.toThrow()
+      expect(() => stat(join(basePath, 'coverage/temp/coverage.json'))).rejects.toThrow()
+      expect(() => stat(join(basePath, 'coverage/report'))).rejects.toThrow()
     })
 
     it('does not create a mapping', async () => {
@@ -60,58 +46,35 @@ describe('src/coverage', () => {
   })
 
   describe('enabled', () => {
-    const path = join(coveragePath, 'enabled')
+    const basePath = join(path, 'enabled')
     let job
 
-    beforeAll(() => {
-      job = jobFactory.fromCmdLine(cwd, [0, 0,
-        `-covTempDir:${join(path, 'temp')}`,
-        `-covReportDir:${join(path, 'report')}`
-      ])
+    beforeAll(async () => {
+      const reportDir = join(basePath, 'report')
+      await createDir(reportDir)
+      job = fromObject(cwd, {
+        reportDir,
+        coverageTempDir: join(basePath, 'coverage/temp'),
+        coverageReportDir: join(basePath, 'coverage/report'),
+        coverage: true
+      })
     })
 
     it('instruments sources', async () => {
-      let triggered = false
-      hook.once('new', childProcess => {
-        triggered = true
-        setTimeout(() => childProcess.emit('close'), 0)
-      })
       await instrument(job)
-      expect(triggered).toStrictEqual(true)
-      const stat = await statAsync(join(path, 'temp/settings/nyc.json'))
-      expect(stat.isFile()).toStrictEqual(true)
+      const nycJsonStat = await stat(join(basePath, 'coverage/temp/settings/nyc.json'))
+      expect(nycJsonStat.isFile()).toStrictEqual(true)
     })
 
     it('generates a report', async () => {
-      let mergeTriggered = false
-      let reportTriggered = false
-      function newChildProcess (childProcess) {
-        if (childProcess.args[0] === 'merge') {
-          mergeTriggered = true
-        } else if (childProcess.args[0] === 'report') {
-          reportTriggered = true
-        }
-        setTimeout(() => childProcess.emit('close'), 0)
-      }
-      hook.on('new', newChildProcess)
       await generateCoverageReport(job)
-      expect(mergeTriggered).toStrictEqual(true)
-      expect(reportTriggered).toStrictEqual(true)
-      hook.off('new', newChildProcess)
+      const reportStat = await stat(join(basePath, 'coverage/report'))
+      expect(reportStat.isDirectory()).toStrictEqual(true)
     })
 
     it('creates a mapping', async () => {
       const coverageMappings = mappings(job)
       expect(coverageMappings.length).toStrictEqual(1)
     })
-  })
-
-  afterAll(() => {
-    expect(log.mock.calls.length).toStrictEqual(0)
-    expect(warn.mock.calls.length).toStrictEqual(0)
-    expect(error.mock.calls.length).toStrictEqual(0)
-    log.mockRestore()
-    warn.mockRestore()
-    error.mockRestore()
   })
 })
