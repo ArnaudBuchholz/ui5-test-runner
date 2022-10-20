174705a7ac3004b12823ad2076d8f9e9dfcb1661
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-10-20 06:18:36 -0400

    testsuite does not make sense with url

--- src/job-mode.js
+++ src/job-mode.js
@@ -1,11 +1,15 @@
 const { UTRError } = require('./error')
 const { $valueSources } = require('./symbols')
 
-function only (job, allowed) {
+function check (job, allowed, forbidden) {
   const valueSources = job[$valueSources]
   const incompatible = Object.keys(valueSources).filter(option => {
     const source = valueSources[option]
-    return source === 'cli' && !allowed.includes(option)
+    return source === 'cli' &&
+    (
+      (allowed && !allowed.includes(option)) ||
+      (forbidden && forbidden.includes(option))
+    )
   })
   if (incompatible.length) {
     throw UTRError.MODE_INCOMPATIBLE_OPTION(incompatible)
@@ -14,10 +18,11 @@ function only (job, allowed) {
 
 function buildAndCheckMode (job) {
   if (job.capabilities) {
-    only(job, ['capabilities', 'cwd', 'port', 'browser', 'parallel', 'reportDir', 'pageTimeout', 'failFast'])
+    check(job, ['capabilities', 'cwd', 'port', 'browser', 'parallel', 'reportDir', 'pageTimeout', 'failFast'])
     return 'capabilities'
   }
   if (job.url && job.url.length) {
+    check(job, undefined, ['testsuite'])
     return 'url'
   }
   return 'legacy'
