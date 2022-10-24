c6e14926a376757e11b288114f9d71c46be7e5f4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-10-24 07:32:07 -0400

    New symbols for probing loop

--- src/symbols.js
+++ src/symbols.js
@@ -1,5 +1,7 @@
 module.exports = {
   $browsers: Symbol('browsers'),
+  $probeUrlsStarted: Symbol('probeUrlsStarted'),
+  $probeUrlsCompleted: Symbol('probeUrlsCompleted'),
   $testPagesStarted: Symbol('testPagesStarted'),
   $testPagesCompleted: Symbol('testPagesCompleted'),
   $valueSources: Symbol('valueSources')
