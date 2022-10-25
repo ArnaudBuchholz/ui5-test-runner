d2c2d5cafb06f3d68e47e2bc4d010c85c0890680
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-10-25 07:22:28 -0400

    Improve logging by changing error message

--- src/error.spec.js
+++ src/error.spec.js
@@ -3,9 +3,15 @@ const { UTRError } = require('./error')
 describe('src/errors', () => {
   it('generates a fully documented error', () => {
     const error = UTRError.NPM_FAILED('test')
-    expect(error.name).toStrictEqual('UTRError')
+    expect(error.name).toStrictEqual('UTRError:NPM_FAILED')
+    expect(error.code).toBeLessThan(0)
+    expect(error.message).toStrictEqual('test')
+  })
+
+  it('generates a generic error', () => {
+    const error = UTRError.NPM_FAILED()
+    expect(error.name).toStrictEqual('UTRError:NPM_FAILED')
     expect(error.code).toBeLessThan(0)
     expect(error.message).toStrictEqual('NPM_FAILED')
-    expect(error.details).toStrictEqual('test')
   })
 })
