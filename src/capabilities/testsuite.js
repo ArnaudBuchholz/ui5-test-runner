02fc9981d9240b953b140b1285d323f06fb1f0d1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-01 06:43:13 -0400

    Isolate tests

--- src/capabilities/testsuite.js
+++
@@ -1,8 +0,0 @@
-window.suite = function () {
-  'use strict'
-  const suite = new window.parent.jsUnitTestSuite() // eslint-disable-line new-cap
-  const path = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/') + 1)
-  suite.addTestPage(`${path}unit/unitTests.qunit.html`)
-  suite.addTestPage(`${path}integration/opaTests.iframe.qunit.html`)
-  return suite
-}
