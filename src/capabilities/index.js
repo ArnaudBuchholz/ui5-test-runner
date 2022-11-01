4def291d06f2e7871ea6cca11365fa59ee270ddc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-11-01 06:45:00 -0400

    Isolate tests

--- src/capabilities/index.js
+++ src/capabilities/index.js
@@ -1,152 +1,14 @@
 'use strict'
 
 const { check, serve, body } = require('reserve')
-const { probe, start, screenshot, stop } = require('../browsers')
+const { probe, start, stop } = require('../browsers')
 const { join } = require('path')
-const { stat, readFile } = require('fs/promises')
 const { getOutput } = require('../output')
 const EventEmitter = require('events')
-const assert = require('assert/strict')
 const { performance } = require('perf_hooks')
 const { cleanDir, allocPromise, filename } = require('../tools')
 const { $browsers } = require('../symbols')
-
-function qUnitEndpoints (data) {
-  const { endpoint } = data
-  if (!this.calls) {
-    this.calls = {}
-  }
-  if (!this.calls[endpoint]) {
-    this.calls[endpoint] = 1
-  } else {
-    ++this.calls[endpoint]
-  }
-  if (endpoint === 'QUnit/done') {
-    assert(this.calls['QUnit/begin'], 'QUnit/begin was triggered')
-    assert(this.calls['QUnit/log'], 'QUnit/log was triggered')
-    assert(this.calls['QUnit/testDone'], 'QUnit/testDone was triggered')
-    return true
-  }
-}
-
-const getTests = job => [{
-  label: 'Loads a page',
-  url: 'basic.html',
-  log: () => {}
-}, {
-  label: 'Local storage (1)',
-  url: 'localStorage.html?value=1',
-  log: ({ initial, modified }) => {
-    assert(initial === undefined, 'The local storage starts empty')
-    assert(modified === '1', 'The local storage can be used')
-  }
-}, {
-  label: 'Local storage (2)',
-  url: 'localStorage.html?value=2',
-  log: ({ initial, modified }) => {
-    assert(initial === undefined, 'The local storage starts empty')
-    assert(modified === '2', 'The local storage can be used')
-  }
-}, {
-  label: 'Timeout (100ms)',
-  url: 'timeout.html?rate=100&wait=1000',
-  log: ({ steps }) => {
-    assert(steps.length > 8, 'The right number of steps is generated')
-  }
-}, {
-  label: 'Timeout (250ms)',
-  url: 'timeout.html?rate=250&wait=1250',
-  log: ({ steps }) => {
-    assert(steps.length > 3, 'The right number of steps is generated')
-  }
-}, {
-  label: 'Console logs',
-  for: capabilities => !!capabilities.console,
-  url: 'console.html',
-  log: async (data, url) => {
-    const pageBrowser = job[$browsers][url]
-    assert(!!pageBrowser)
-    const { promise, resolve, reject } = allocPromise()
-    function waitForConsoleLogs () {
-      if (pageBrowser.console.count === 5) {
-        pageBrowser.console.flush
-          .then(async () => {
-            const jsonl = (await readFile(join(pageBrowser.reportDir, 'console.jsonl'))).toString()
-            const logs = jsonl
-              .split('\n')
-              .filter(line => !!line)
-              .map(line => {
-                const json = JSON.parse(line)
-                delete json.t
-                return json
-              })
-            try {
-              assert.deepEqual(logs, [{
-                api: 'log',
-                args: ['A simple string']
-              }, {
-                api: 'log',
-                args: ['complex parameters', 1, true, { property: 'value' }]
-              }, {
-                api: 'warning',
-                args: ['A warning']
-              }, {
-                api: 'error',
-                args: ['An error']
-              }, {
-                api: 'info',
-                args: ['An info']
-              }])
-              resolve()
-            } catch (e) {
-              reject(e)
-            }
-          })
-      } else {
-        setTimeout(waitForConsoleLogs, 250)
-      }
-    }
-    waitForConsoleLogs()
-    return promise
-  }
-}, {
-  label: 'Screenshot',
-  for: capabilities => !!capabilities.screenshot,
-  url: 'screenshot.html',
-  log: async (data, url) => {
-    const fileName = await screenshot(job, url, 'screenshot')
-    const fileInfo = await stat(fileName)
-    assert(fileInfo.isFile(), 'The file was generated')
-    assert(fileInfo.size > 1024, 'The file contains something')
-  }
-}, {
-  label: 'Scripts (QUnit)',
-  for: capabilities => !!capabilities.scripts,
-  url: 'qunit.html',
-  scripts: ['qunit-intercept.js', 'post.js', 'qunit-hooks.js'],
-  endpoint: qUnitEndpoints
-}, {
-  label: 'Scripts (TestSuite)',
-  for: capabilities => !!capabilities.scripts,
-  url: 'testsuite.html',
-  scripts: ['post.js', 'qunit-redirect.js'],
-  endpoint: function (data) {
-    assert(data.endpoint === 'addTestPages', 'addTestPages was triggered')
-    assert(data.body.length === 2, 'Two pages received')
-    const pages = [
-      '/unit/unitTests.qunit.html',
-      '/integration/opaTests.iframe.qunit.html'
-    ]
-    pages.forEach((page, index) => assert(data.body[index] === page, page))
-    return true
-  }
-}, {
-  label: 'Scripts (External QUnit)',
-  for: capabilities => !!capabilities.scripts,
-  url: 'https://ui5.sap.com/test-resources/sap/m/demokit/orderbrowser/webapp/test/unit/unitTests.qunit.html',
-  scripts: ['qunit-intercept.js', 'post.js', 'qunit-hooks.js'],
-  endpoint: qUnitEndpoints
-}]
+const tests = require('./tests')
 
 async function capabilities (job) {
   const output = getOutput(job)
@@ -177,19 +39,10 @@ async function capabilities (job) {
       mappings: [
         require('../cors'), {
           method: 'POST',
-          match: '^/log$',
-          custom: async (request, response) => {
-            const listenerIndex = request.headers.referer.match(/\blistener=(\d+)/)[1]
-            const listener = listeners[listenerIndex]
-            listener.emit('log', JSON.parse(await body(request)))
-            response.writeHead(200)
-            response.end()
-          }
-        }, {
-          method: 'POST',
           match: '^/_/(.*)',
           custom: async (request, response, endpoint) => {
-            const listenerIndex = request.headers['x-page-url'].match(/\blistener=(\d+)/)[1]
+            const { referer, 'x-page-url': xPageUrl } = request.headers
+            const listenerIndex = (xPageUrl || referer).match(/\blistener=(\d+)/)[1]
             const listener = listeners[listenerIndex]
             listener.emit('endpoint', {
               endpoint,
@@ -212,7 +65,7 @@ async function capabilities (job) {
 
     job.status = 'Running tests'
 
-    const filteredTests = getTests(job).filter((test) => !test.for || test.for(job.browserCapabilities))
+    const filteredTests = tests.filter((test) => !test.for || test.for(job.browserCapabilities))
     output.wrap(() => console.log('Number of tests :', filteredTests.length))
 
     let errors = 0
@@ -227,7 +80,7 @@ async function capabilities (job) {
         }
         return
       }
-      const { label, url, log, scripts, endpoint } = filteredTests.shift()
+      const { label, url, scripts, endpoint = () => {} } = filteredTests.shift()
 
       const listenerIndex = listeners.length
       const listener = new EventEmitter()
@@ -236,7 +89,7 @@ async function capabilities (job) {
       if (url.startsWith('http')) {
         pageUrl = url
       } else {
-        pageUrl = `http://localhost:${job.port}/${url}`
+        pageUrl = `http://localhost:${job.port}/tests/${url}`
       }
       if (url.includes('?')) {
         pageUrl += `&listener=${listenerIndex}`
@@ -267,32 +120,25 @@ async function capabilities (job) {
         await next()
       }
 
-      listener.on('log', async data => {
+      const context = {
+        job
+      }
+      listener.on('endpoint', async data => {
         try {
-          await log(data, pageUrl)
-          done()
+          if (await endpoint.call(context, data, pageUrl) !== false) {
+            done()
+          }
         } catch (e) {
           done(e)
         }
       })
 
-      if (endpoint) {
-        const context = {}
-        listener.on('endpoint', async data => {
-          try {
-            if (await endpoint.call(context, data, pageUrl) === true) {
-              done()
-            }
-          } catch (e) {
-            done(e)
-          }
-        })
-      }
-
       start(job, pageUrl, scripts)
         .catch(reason => done(reason))
         .then(() => {
-          done('Failed')
+          if (!context.stopExpected) {
+            done('Failed')
+          }
         })
     }
 
