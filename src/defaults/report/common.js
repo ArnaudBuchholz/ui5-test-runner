26061f1fd17ce279e42b3c588e1c57a8db85fd8f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-07 15:51:32 -0500

    Common tools (static and progress reports)

---
+++ src/defaults/report/common.js
@@ -0,0 +1,36 @@
+(function () {
+  const report = {}
+
+  report.elapsed = function (start, end = Date.now()) {
+    if (typeof end === 'string') {
+      end = new Date(end).getTime()
+    }
+    if (typeof start === 'string') {
+      start = new Date(start).getTime()
+    }
+    const ms = end - start
+    if (isNaN(ms)) {
+      return '-'
+    }
+    if (ms > 5000) {
+      const mins = Math.floor(ms / 60000)
+      const secs = Math.floor((ms % 60000) / 1000)
+      return `${mins.toString().padStart(2, 0)}:${secs.toString().padStart(2, 0)}`
+    }
+    return `${ms} ms`
+  }
+
+  let setReady
+  report.ready = new Promise(resolve => {
+    setReady = resolve
+  })
+
+  window.addEventListener('load', () => {
+    const safebind = punybind.use({
+      compiler: punyexpr
+    })
+    safebind(document.body).then(setReady)
+  })
+
+  window.report = report
+}())
