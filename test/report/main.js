b6bedfaaff96e55058711691ffd1ccfe1bc08aad
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-01-19 18:21:19 +0000

    Build the report body

--- test/report/main.js
+++ test/report/main.js
@@ -1,13 +1,22 @@
-module.exports = (request, response) => {
+const { join } = require('path')
+const { readFile } = require('fs/promises')
+
+module.exports = async (request, response) => {
   const { referer } = request.headers
-  if (referer.endsWith('report.html')) {
-    response.writeHead(308, {
-      location: '/_/report/report.js'
-    })
-  } else {
+  if (!referer || referer.endsWith('progress.html')) {
     response.writeHead(308, {
       location: '/_/report/progress.js'
     })
+  } else {
+    response.writeHead(200, {
+      'content-type': 'text/javascript'
+    })
+    response.write(`const module = {}
+`)
+    response.write((await readFile(join(__dirname, 'job.js'))).toString())
+    response.write(`const job = module.exports
+`)
+    response.write((await readFile(join(__dirname, '../../src/defaults/report/main.js'))).toString())
   }
   response.end()
 }
