279eae0ea724692248f6f9c4fecab54293c7b9dd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-01-19 05:38:16 -0500

    Offers test details

--- src/get-job-progress.js
+++ src/get-job-progress.js
@@ -1,35 +1,68 @@
 'use strict'
 
+const send = (response, obj) => {
+  let json
+  if (typeof obj !== 'string') {
+    json = JSON.stringify(obj, undefined, 2)
+  } else {
+    json = obj
+  }
+  const length = (new TextEncoder().encode(json)).length
+  response.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Content-Length': length
+  })
+  response.end(json)
+}
+
+const notFound = response => {
+  response.writeHead(404)
+  response.end()
+}
+
 module.exports = {
-  async getJobProgress (job, request, response, pageId) {
-    let json
+  async getJobProgress (job, request, response, pageId, testId) {
     if (pageId) {
-      const pageUrl = Object.keys(job.qunitPages).find(url => job.qunitPages[url].id === pageId)
-      if (pageUrl === undefined) {
-        response.writeHead(404)
-        response.end()
-        return
+      const url = Object.keys(job.qunitPages).find(pageUrl => job.qunitPages[pageUrl].id === pageId)
+      if (!url) {
+        return notFound(response)
       }
-      json = JSON.stringify({
-        url: pageUrl,
-        ...job.qunitPages[pageUrl]
-      }, undefined, 2)
-    } else {
-      json = JSON.stringify({
-        ...job,
-        status: job.status
-      }, (key, value) => {
-        if (key === 'modules') {
-          return undefined
+      const qunitPage = { url, ...job.qunitPages[url] }
+      if (!testId) {
+        return send(response, JSON.stringify(qunitPage, (key, value) => {
+          if (key === 'logs') {
+            return undefined
+          }
+          return value
+        }, 2))
+      }
+      let test
+      let moduleName
+      qunitPage.modules.every(module => module.tests.every(candidate => {
+        if (candidate.testId === testId) {
+          moduleName = module.name
+          test = candidate
+          return false
         }
-        return value
-      }, 2)
+        return true
+      }))
+      if (!test) {
+        return notFound(response)
+      }
+      return send(response, {
+        url,
+        module: moduleName,
+        ...test
+      })
     }
-    const length = (new TextEncoder().encode(json)).length
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-      'Content-Length': length
-    })
-    response.end(json)
+    send(response, JSON.stringify({
+      ...job,
+      status: job.status
+    }, (key, value) => {
+      if (key === 'modules') {
+        return undefined
+      }
+      return value
+    }, 2))
   }
 }
