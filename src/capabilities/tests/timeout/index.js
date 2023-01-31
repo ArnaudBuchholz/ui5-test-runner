7f3c080f7ebd02223c4f0bdbe26eee27f327e0a1
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-01-31 13:36:18 +0000

    Compatibility with node 14

--- src/capabilities/tests/timeout/index.js
+++ src/capabilities/tests/timeout/index.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const assert = require('assert/strict')
+const assert = require('assert')
 
 module.exports = [{
   label: 'Timeout (100ms)',
   url: 'timeout/index.html?rate=100&wait=1000',
   endpoint: ({ body }) => {
     const { steps } = body
-    assert(steps.length > 8, 'The right number of steps is generated')
+    assert.ok(steps.length > 8, 'The right number of steps is generated')
   }
 }, {
   label: 'Timeout (250ms)',
   url: 'timeout/index.html?rate=250&wait=1250',
   endpoint: ({ body }) => {
     const { steps } = body
-    assert(steps.length > 3, 'The right number of steps is generated')
+    assert.ok(steps.length > 3, 'The right number of steps is generated')
   }
 }]
