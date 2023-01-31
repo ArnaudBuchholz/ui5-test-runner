39636e66a79a5551ae96d7aafed671d3dfc93290
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-01-31 13:36:13 +0000

    Compatibility with node 14

--- src/capabilities/tests/screenshot/index.js
+++ src/capabilities/tests/screenshot/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const assert = require('assert/strict')
+const assert = require('assert')
 const { screenshot } = require('../../../browsers')
 const { stat } = require('fs/promises')
 
@@ -12,7 +12,7 @@ module.exports = {
     const { job } = this
     const fileName = await screenshot(job, url, 'screenshot')
     const fileInfo = await stat(fileName)
-    assert(fileInfo.isFile(), 'The file was generated')
-    assert(fileInfo.size > 1024, 'The file contains something')
+    assert.ok(fileInfo.isFile(), 'The file was generated')
+    assert.ok(fileInfo.size > 1024, 'The file contains something')
   }
 }
