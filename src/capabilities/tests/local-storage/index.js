d3d63bf5cff592dbd57190e90b0776af9c5f4052
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-01-31 13:36:11 +0000

    Compatibility with node 14

--- src/capabilities/tests/local-storage/index.js
+++ src/capabilities/tests/local-storage/index.js
@@ -1,21 +1,21 @@
 'use strict'
 
-const assert = require('assert/strict')
+const assert = require('assert')
 
 module.exports = [{
   label: 'Local storage (1)',
   url: 'local-storage/index.html?value=1',
   endpoint: ({ body }) => {
     const { initial, modified } = body
-    assert(initial === undefined, 'The local storage starts empty')
-    assert(modified === '1', 'The local storage can be used')
+    assert.ok(initial === undefined, 'The local storage starts empty')
+    assert.ok(modified === '1', 'The local storage can be used')
   }
 }, {
   label: 'Local storage (2)',
   url: 'local-storage/index.html?value=2',
   endpoint: ({ body }) => {
     const { initial, modified } = body
-    assert(initial === undefined, 'The local storage starts empty')
-    assert(modified === '2', 'The local storage can be used')
+    assert.ok(initial === undefined, 'The local storage starts empty')
+    assert.ok(modified === '2', 'The local storage can be used')
   }
 }]
