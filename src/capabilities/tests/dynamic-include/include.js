488d2bb948661da80d6fb51190bd9c68c345de0e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-02-01 18:04:55 +0000

    Assess dynamic include

---
+++ src/capabilities/tests/dynamic-include/include.js
@@ -0,0 +1,3 @@
+const xhr = new XMLHttpRequest()
+xhr.open('POST', '/_/log')
+xhr.send('{}')
---
+++ src/capabilities/tests/dynamic-include/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  label: 'Dynamic include',
+  url: 'dynamic-include/index.html'
+}
