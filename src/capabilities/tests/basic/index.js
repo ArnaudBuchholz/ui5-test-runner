27b1998f5f606bcf8fb63d2c1a2530bcdf00f027
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-02 12:45:11 -0500

    Testing a basic UI5 example

--- src/capabilities/tests/basic/index.js
+++ src/capabilities/tests/basic/index.js
@@ -1,6 +1,14 @@
 'use strict'
 
-module.exports = {
+const assert = require('assert')
+
+module.exports = [{
   label: 'Loads a page',
   url: 'basic/index.html'
-}
+}, {
+  label: 'Loads a UI5 example',
+  url: 'basic/ui5.html',
+  endpoint: ({ body }) => {
+    assert.strictEqual(body['sap.m.Button'], true);
+  }
+}]
