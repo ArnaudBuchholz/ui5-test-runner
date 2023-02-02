c3bd2e380e722ebd36eb783a3e36afb5f6661830
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-02 07:22:24 -0500

    Keep the tests 'name'

--- src/capabilities/tests/index.js
+++ src/capabilities/tests/index.js
@@ -2,5 +2,15 @@ const { readdirSync } = require('fs')
 
 module.exports = readdirSync(__dirname)
   .filter(name => !name.endsWith('.js'))
-  .map(name => require(`./${name}`))
+  .map(name => {
+    const tests = require(`./${name}`)
+    if (Array.isArray(tests)) {
+      tests.forEach((test, index) => {
+        test.name = `${name}:${index}`
+      })
+    } else {
+      tests.name = name
+    }
+    return tests
+  })
   .flat()
