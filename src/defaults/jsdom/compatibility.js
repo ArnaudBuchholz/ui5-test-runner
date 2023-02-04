fcecb319bb57660bc63da6b142e811b56ab02142
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-04 12:55:44 -0500

    Moves error handling Improve request logging

--- src/defaults/jsdom/compatibility.js
+++ src/defaults/jsdom/compatibility.js
@@ -16,20 +16,31 @@ function wrapXHR (window) {
   const { XMLHttpRequest } = window
   const { open } = XMLHttpRequest.prototype
   XMLHttpRequest.prototype.open = function (method, url, asynchronous) {
-    window[$networkWriter].append({
-      method,
-      url,
-      status: 'UNK'
+    this.addEventListener('readystatechange', () => {
+      if (this.readyState === XMLHttpRequest.DONE) {
+        const { status } = this
+        window[$networkWriter].append({
+          method,
+          url,
+          status
+        })
+      }
     })
     return open.call(this, method, url, asynchronous)
   }
 }
 
 module.exports = window => {
+  window.addEventListener('error', event => {
+    const { message, filename, lineno, colno } = event
+    window.console.error(`${filename}@${lineno}:${colno}: ${message}`)
+  })
+
   window.performance.timing = {
     navigationStart: new Date().getTime(),
     fetchStart: new Date().getTime()
   }
   window.matchMedia = window.matchMedia || fakeMatchMedia
+
   wrapXHR(window)
 }
