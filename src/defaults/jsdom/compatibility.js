2f85811c6667eb779414f58fb56d24d517c46ef7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-04 01:21:11 -0500

    Improve network traces for jsdom

--- src/defaults/jsdom/compatibility.js
+++ src/defaults/jsdom/compatibility.js
@@ -1,3 +1,7 @@
+const {
+  $networkWriter
+} = require('./symbols')
+
 const noop = () => {}
 
 function fakeMatchMedia () {
@@ -8,10 +12,24 @@ function fakeMatchMedia () {
   }
 }
 
+function wrapXHR (window) {
+  const { XMLHttpRequest } = window
+  const { open } = XMLHttpRequest.prototype
+  XMLHttpRequest.prototype.open = function (method, url, asynchronous) {
+    window[$networkWriter].append({
+      method,
+      url,
+      status: 'UNK'
+    })
+    return open.call(this, method, url, asynchronous)
+  }
+}
+
 module.exports = window => {
   window.performance.timing = {
     navigationStart: new Date().getTime(),
     fetchStart: new Date().getTime()
   }
   window.matchMedia = window.matchMedia || fakeMatchMedia
+  wrapXHR(window)
 }
