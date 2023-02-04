25e901a083e974ee0905906b1f2c3fa34fde7b62
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-04 00:28:23 -0500

    Adds missing matchMedia for UI5

--- src/defaults/jsdom/compatibility.js
+++ src/defaults/jsdom/compatibility.js
@@ -1,6 +1,17 @@
+const noop = () => {}
+
+function fakeMatchMedia () {
+  return {
+    matches: false,
+    addListener: noop,
+    removeListener: noop
+  }
+}
+
 module.exports = window => {
   window.performance.timing = {
     navigationStart: new Date().getTime(),
     fetchStart: new Date().getTime()
   }
+  window.matchMedia = window.matchMedia || fakeMatchMedia
 }
