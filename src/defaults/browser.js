d2a9949aff8f2f4459a8765a3066ec793472daff
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-09 07:57:54 -0500

    Attempt to unify browser commands

---
+++ src/defaults/browser.js
@@ -0,0 +1,96 @@
+const { readFile, writeFile } = require('fs/promises')
+const { join } = require('path')
+const { Command } = require('commander')
+const { buildCsvWriter } = require('../csv-writer')
+
+const noop = () => {}
+
+module.exports = ({
+  name,
+  options,
+  flush = noop,
+  beforeExit = noop,
+  screenshot,
+  capabilities,
+  run,
+  error = noop
+}) => {
+  const command = new Command()
+  command
+    .name(`ui5-test-runner/@/${name}`)
+    .description(`Browser instantiation command for ${name}`)
+    .helpOption(false)
+  options(command)
+
+  const append = () => { }
+  let consoleWriter = { ready: Promise.resolve(), append }
+  let networkWriter = { ready: Promise.resolve(), append }
+
+  let stopping = false
+
+  async function exit (code) {
+    if (stopping) {
+      return
+    }
+    stopping = true
+    await flush()
+    await Promise.all([consoleWriter.ready, networkWriter.ready])
+    await beforeExit()
+    process.exit(code)
+  }
+
+  process.on('message', async message => {
+    const { command } = message
+    try {
+      if (command === 'stop') {
+        await exit(0)
+      } else if (command === 'screenshot') {
+        if (screenshot && await screenshot(message.filename)) {
+          process.send(message)
+        } else {
+          throw new Error('screenshot command failed')
+        }
+      }
+    } catch (e) {
+      console.error(e)
+      exit(-2)
+    }
+  })
+
+  if (process.argv.length !== 3) {
+    command.outputHelp()
+    return exit(0)
+  }
+
+  async function main () {
+    const settings = JSON.parse((await readFile(process.argv[2])).toString())
+    command.parse(settings.args, { from: 'user' })
+    const options = command.opts()
+
+    if (settings.capabilities) {
+      const attributes = await capabilities(settings)
+      await writeFile(settings.capabilities, JSON.stringify(attributes))
+      return exit(0)
+    }
+
+    consoleWriter = buildCsvWriter(join(settings.dir, 'console.csv'))
+    networkWriter = buildCsvWriter(join(settings.dir, 'network.csv'))
+
+    await run({
+      settings,
+      options,
+      consoleWriter,
+      networkWriter,
+      exit
+    })
+  }
+
+  main()
+    .catch(async e => {
+      if (!stopping) {
+        await error(e)
+        console.error(e)
+      }
+      exit(-1)
+    })
+}
