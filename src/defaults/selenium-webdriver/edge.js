c1c06663850ad80a0e7f86444ea9b2d943d872c9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-11 01:05:38 -0500

    Edge implementation

---
+++ src/defaults/selenium-webdriver/edge.js
@@ -0,0 +1,31 @@
+const { join } = require('path')
+
+module.exports = async ({
+  seleniumWebdriver,
+  settings,
+  options,
+  loggingPreferences,
+  $capabilities
+}) => {
+  const { Browser, Builder } = seleniumWebdriver
+  const edge = require(join(settings.modules['selenium-webdriver'], 'edge'))
+
+  const edgeOptions = new edge.Options()
+  edgeOptions.setLoggingPrefs(loggingPreferences)
+
+  const builder = new Builder()
+    .forBrowser(Browser.EDGE)
+    .setEdgeOptions(edgeOptions)
+
+  if (options.server) {
+    builder.usingServer(options.server)
+  }
+
+  const driver = await builder.build()
+
+  driver[$capabilities] = {
+    traces: ['network'] // console misses info/log
+  }
+
+  return driver
+}
