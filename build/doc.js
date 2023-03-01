bbe962c0725a97f99e0e1c26cf965649de44fb4b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-02-28 23:40:28 -0500

    120

--- build/doc.js
+++ build/doc.js
@@ -6,7 +6,7 @@ async function main () {
   const usagePath = join(__dirname, '../docs/usage.md')
   const usageMd = (await readFile(usagePath)).toString()
   const command = getCommand(process.cwd())
-  process.stdout.columns = 110
+  process.stdout.columns = 120
   const help = command.helpInformation()
   const [, reducedHelp] = help.match(/Options:\n((?:.|\n)*)/m)
   await writeFile(usagePath, usageMd.replace(/```text(?:.|\n)*```/, '```text\n' + reducedHelp + '```'))
