3795fc77019cf62e532075f758697ce21139d6b6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-02-06 07:01:53 -0500

    test(screenshot): assessing focus issue (#72)

--- src/capabilities/tests/screenshot/index.js
+++ src/capabilities/tests/screenshot/index.js
@@ -8,11 +8,17 @@ module.exports = {
   label: 'Screenshot',
   for: capabilities => !!capabilities.screenshot,
   url: 'screenshot/index.html',
-  endpoint: async function (_, url) {
+  endpoint: async function ({ body: { step, current } }, url) {
     const { job } = this
-    const fileName = await screenshot(job, url, 'screenshot')
-    const fileInfo = await stat(fileName)
-    assert.ok(fileInfo.isFile(), 'The file was generated')
-    assert.ok(fileInfo.size > 1024, 'The file contains something')
+    if (step === 'screenshot') {
+      const fileName = await screenshot(job, url, 'screenshot')
+      const fileInfo = await stat(fileName)
+      assert.ok(fileInfo.isFile(), 'The file was generated')
+      assert.ok(fileInfo.size > 1024, 'The file contains something')
+      return false
+    } else {
+      assert.strictEqual(current, 'first')
+      return true
+    }
   }
 }
