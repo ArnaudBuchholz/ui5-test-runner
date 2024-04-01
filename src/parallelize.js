a002332bae7233ff52ecd966642f526cd8e8b3e5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-04-01 16:51:17 -0400

    fix(parallelize): ensure edge cases are handled (wip)

--- src/parallelize.js
+++ src/parallelize.js
@@ -1,5 +1,11 @@
 const { allocPromise } = require('./tools')
 
+function end (task) {
+  if (--task.active === 0) {
+    task.resolve()
+  }
+}
+
 async function run (task) {
   const {
     method,
@@ -8,15 +14,15 @@ async function run (task) {
     started,
     completed,
     stop,
-    resolve,
     reject
   } = task
   const { length } = list
   if (stop || completed === length || started === length) {
+    end(task)
     return
   }
-  const active = ++task.active
-  if (active < parallel && length - started > 1) {
+  if (task.active < parallel && length - started > 1) {
+    ++task.active
     run(task)
   }
   const index = task.started++
@@ -28,24 +34,24 @@ async function run (task) {
     reject(error)
   }
   ++task.completed
+  ++task.active
   run(task)
-  if (--task.active === 0) {
-    resolve()
-  }
+  end(task)
 }
 
 module.exports = function parallelize (method, list, parallel) {
   const { promise, resolve, reject } = allocPromise()
-  run({
+  const task = {
     method,
     list,
     parallel,
     started: 0,
     completed: 0,
-    active: 0,
+    active: 1,
     stop: false,
     resolve,
     reject
-  })
+  }
+  run(task)
   return promise
 }
--- src/parallelize.spec.js
+++ src/parallelize.spec.js
@@ -3,6 +3,12 @@ const parallelize = require('./parallelize')
 describe('src/parallelize', () => {
   const list = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
+  it('handles empty list', async () => {
+    await expect(parallelize(() => {
+      throw new Error('Should not have any item to process')
+    }, [], 4)).resolves.toBeUndefined()
+  }, 100)
+
   it('processes all the values', async () => {
     const processed = []
     await parallelize((value, index, array) => {
@@ -21,7 +27,7 @@ describe('src/parallelize', () => {
       let maxActive = 0
       await parallelize(async (value) => {
         ++active
-        maxActive = Math.max(parallel, active)
+        maxActive = Math.max(maxActive, active)
         processed.push(value)
         await new Promise(resolve => setTimeout(resolve, 10))
         --active
@@ -43,6 +49,26 @@ describe('src/parallelize', () => {
     }, partial, 1)
     expect(processed.length).toStrictEqual(list.length)
     expect(processed.sort()).toStrictEqual(list)
+  })
+
+  it('augments parallelism (if needed) when the list is growing', async () => {
+    const processed = []
+    const partial = ['a']
+    let active = 0
+    let maxActive = 0
+    await parallelize(async (value) => {
+      ++active
+      maxActive = Math.max(maxActive, active)
+      if (value === 'a') {
+        partial.push('b', 'c')
+      }
+      processed.push(value)
+      await new Promise(resolve => setTimeout(resolve, 10))
+      --active
+    }, partial, 2)
+    expect(processed.length).toStrictEqual(3)
+    expect(processed.sort()).toStrictEqual('abc'.split(''))
+    expect(maxActive).toStrictEqual(2)
   })
 
   it('fails if one task fails', async () => {
