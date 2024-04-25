364464e07bd6ab2b33ba695b83599c15a73a6181
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-04-25 08:11:13 -0400

    feat(coverage): externalize ui5 scanner #85

---
+++ src/defaults/scan-ui5.js
@@ -0,0 +1,18 @@
+module.exports = async function scanUI5 (url, onFolder, onFile) {
+  if (url.match(/\/((?:test-)?resources\/.*)/)) {
+    return // ignore UI5 resources
+  }
+  const html = await (await fetch(url)).text()
+  const items = [...html.matchAll(/<a href="([^"]+)" class="icon/ig)]
+    .map(([_, item]) => item)
+    .filter(item => item.endsWith('/') || item.endsWith('.js'))
+  await onFolder(items.length)
+  for (const item of items) {
+    const itemUrl = new URL(item, url).toString()
+    if (item.endsWith('/')) {
+      await scanUI5(itemUrl, onFolder, onFile)
+    } else {
+      await onFile(itemUrl, await (await fetch(itemUrl + '?instrument=true')).text())
+    }
+  }
+}
