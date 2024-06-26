ebc677d06d75064419b0e43d2eebd05235be2354
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-06-26 15:56:08 -0400

    feat: add a test case with shim #95

---
+++ test/sample.js/webapp/test/integration/opaTests.qunit-shim.js
@@ -0,0 +1,33 @@
+sap.ui.loader.config({
+  shim: {
+    "sap/ui/qunit/qunit-junit": {
+      deps: ["sap/ui/thirdparty/qunit"]
+    },
+    "sap/ui/qunit/qunit-coverage": {
+      deps: ["sap/ui/thirdparty/qunit"]
+    },
+    "sap/ui/thirdparty/sinon-qunit": {
+      deps: ["sap/ui/thirdparty/qunit", "sap/ui/thirdparty/sinon"]
+    },
+    "sap/ui/qunit/sinon-qunit-bridge": {
+      deps: ["sap/ui/thirdparty/qunit", "sap/ui/thirdparty/sinon-4"]
+    }
+  }
+});
+
+window.QUnit ??= {};
+window.QUnit.config ??= {};
+window.QUnit.config.autostart ??= false;
+
+sap.ui.require([
+  "sap/ui/thirdparty/qunit",
+  "sap/ui/qunit/qunit-junit",
+  "sap/ui/qunit/qunit-coverage"
+], function () {
+  sap.ui.require([
+    'integration/PageJourney',
+    'integration/HelloJourney'
+  ], function () {
+    QUnit.start()
+  })
+});
