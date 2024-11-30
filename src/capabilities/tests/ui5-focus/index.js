57f1dab0bba076e9f7b2101744b77aabcd1059e7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-11-29 22:26:41 -0500

    fix: exclude ui5-focus test for JSDOM

--- src/capabilities/tests/ui5-focus/index.js
+++ src/capabilities/tests/ui5-focus/index.js
@@ -4,6 +4,7 @@ const assert = require('assert')
 
 module.exports = [{
   label: 'UI5 focus handling',
+  for: capabilities => !capabilities.modules.includes('jsdom'), // does not work on JSDOM
   url: 'ui5-focus/index.html',
   endpoint: ({ body }) => {
     assert.strictEqual(body['is-focus-set'], true)
