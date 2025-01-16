a764342ff8a192fe72bd519a82024969bacb6528
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-01-16 17:01:49 -0500

    feat: improve usage documentation

--- build/doc.js
+++ build/doc.js
@@ -11,9 +11,50 @@ async function usage () {
   const command = getCommand(process.cwd())
   const help = command.helpInformation()
   const [, reducedHelp] = help.match(/Options:\r?\n((?:.|\r?\n)*)/m)
+  const textLines = reducedHelp.split(/\r?\n/)
+
+  const optionWidth = textLines[0].indexOf('output the version number')
+  const arrayLines = textLines.reduce((result, line) => {
+    const option = line.substring(0, optionWidth)
+    const description = line.substring(optionWidth)
+    if (option.trim()) {
+      if (result.length) {
+        let { description } = result.at(-1)
+        const [, defaultValue] = description.match(/\(default: (.*)\)/) ?? []
+        if (defaultValue) {
+          result.at(-1).default = defaultValue.trim()
+          description = description.substring(0, description.indexOf('(default:')).trim()
+        }
+        let [modes] = description.match(/\[(💻|🔗|🧪)+\]|🧪|🔗/) ?? []
+        if (modes) {
+          description = description.substring(modes.length).trim()
+          if (modes.startsWith('[')) {
+            modes = modes.substring(1, modes.length - 1)
+          }
+          result.at(-1).modes = modes
+        }
+        result.at(-1).description = description
+      }
+      result.push({
+        option: option.trim(),
+        description: description.trim()
+      })
+    } else {
+      result.at(-1).description += ' ' + description.trim()
+    }
+    return result
+  }, [])
+
+  const forMd = (value) => value.replace(/\|/g, '\\|').replace(/</g, '\\<')
+
   await writeFile(usagePath, usageMd
-    .replace(/```text(?:.|\r?\n)*```/, '```text\n' + reducedHelp + '```')
-    .replace(/\r\n/g, '\n')
+    .replace(
+      /\|-\|-\|-\|-\|(?:.|\r?\n)*\|\|\|\|\|/,
+      '|-|-|-|-|\n' +
+      arrayLines
+        .map((line) => `|${forMd(line.option)}|${line.modes ?? ''}|${forMd(line.description)}|${line.default ? `\`${forMd(line.default)}\`` : ''}|`)
+        .join('\n') + '\n|||||'
+    )
   )
 }
 
