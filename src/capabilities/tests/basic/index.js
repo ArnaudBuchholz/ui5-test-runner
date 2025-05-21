1206798a06ebc3562919f459842299d5c22fed90
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-05-21 09:57:03 -0400

    fix: disable tests for JSDOM

--- src/capabilities/tests/basic/index.js
+++ src/capabilities/tests/basic/index.js
@@ -7,12 +7,14 @@ module.exports = [{
   url: 'basic/index.html'
 }, {
   label: 'Loads a UI5 example',
+  for: capabilities => !capabilities.modules.includes('jsdom'), // does not work anymore on JSDOM
   url: 'basic/ui5.html',
   endpoint: ({ body }) => {
     assert.strictEqual(body['sap.m.Button'], true)
   }
 }, {
   label: 'Loads a UI5 inside an iframe',
+  for: capabilities => !capabilities.modules.includes('jsdom'), // does not work anymore on JSDOM
   url: 'basic/iframe.html',
   endpoint: ({ body }) => {
     assert.strictEqual(body['sap.m.Button'], true)
