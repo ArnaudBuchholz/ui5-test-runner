cc22131d299c9f53b60e45dce328d40b0102ccde
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-06-02 13:30:17 -0400

    test: add happy-dom console (#130)

--- src/defaults/happy-dom.js
+++ src/defaults/happy-dom.js
@@ -21,7 +21,34 @@ require('./browser')({
   }) {
     const happyDom = require(modules['happy-dom'])
     const { Browser } = happyDom;
-    const browser = new Browser();
+    const browser = new Browser({
+      console: {
+        error: (...args) => console.log(JSON.stringify({
+          timestamp: new Date().toISOString(),
+          channel: 'console',
+          type: 'error',
+          message: args.join(' ')
+        })),
+        warn: (...args) => console.log(JSON.stringify({
+          timestamp: new Date().toISOString(),
+          channel: 'console',
+          type: 'warning',
+          message: args.join(' ')
+        })),
+        info: (...args) => console.log(JSON.stringify({
+          timestamp: new Date().toISOString(),
+          channel: 'console',
+          type: 'info',
+          message: args.join(' ')
+        })),
+        log: (...args) => console.log(JSON.stringify({
+          timestamp: new Date().toISOString(),
+          channel: 'console',
+          type: 'log',
+          message: args.join(' ')
+        }))
+      }
+    });
     const page = browser.newPage();
     scripts.forEach(script => page.mainFrame.window.eval(script));
     page.goto(url);
