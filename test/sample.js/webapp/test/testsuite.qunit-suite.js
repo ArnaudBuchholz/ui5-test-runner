fee7561a761f011d25e3f849ee270fd6cf74e108
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-08-04 16:02:30 -0400

    test: adding test starter example

---
+++ test/sample.js/webapp/test/testsuite.qunit-suite.js
@@ -0,0 +1,28 @@
+sap.ui.define(() => {
+	"use strict";
+
+	return {
+		name: "QUnit test suite for the UI5 Application: sample.js",
+		defaults: {
+			page: "ui5://test-resources/samplejs/testsuite={suite}&test={name}",
+			qunit: {
+				version: 2
+			},
+			sinon: {
+				version: 4
+			},
+			ui5: {
+				language: "EN",
+				theme: "sap_horizon"
+			},
+		},
+		tests: {
+			"unit/unitTests": {
+				title: "Unit tests for samples.js"
+			},
+			"integration/opaTests": {
+				title: "OPA tests for samples.js"
+			}
+		}
+	};
+});
