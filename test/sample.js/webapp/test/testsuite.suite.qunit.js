0790803c9f752b834a56d12fae4ef52e5d83f531
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-08-05 08:24:36 -0400

    test: implemented new test suite format

--- test/sample.js/webapp/test/testsuite.suite.qunit.js
+++ test/sample.js/webapp/test/testsuite.suite.qunit.js
@@ -4,7 +4,7 @@ sap.ui.define(() => {
 	return {
 		name: "QUnit test suite for the UI5 Application: sample.js",
 		defaults: {
-			page: "ui5://test-resources/samplejs/testsuite={suite}&test={name}",
+			page: "ui5://test-resources/samplejs/test.qunit.html?testsuite={suite}&test={name}",
 			qunit: {
 				version: 2
 			},
