0aa02d8f422d779eab3df617d2ed32df4271193f
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-09-26 13:52:46 -0400

    fix: improve handle documentation

--- src/handle.js
+++ src/handle.js
@@ -1,4 +1,4 @@
-const { ServerResponse } = require('http')
+const { ServerResponse, ClientRequest } = require('node:http')
 
 module.exports = {
   describeHandle (handle) {
@@ -6,19 +6,37 @@ module.exports = {
     let label = className
     if (['TLSSocket', 'Socket'].includes(className)) {
       if (handle._httpMessage instanceof ServerResponse) {
-        const { path, method, host, protocol } = handle._httpMessage.req
-        label += ` HTTP ${method} ${protocol}//${host}${path}`
+        const { method, url } = handle._httpMessage.req
+        label += ` IncomingRequest ${method} ${url}`
+      } else if (handle._httpMessage instanceof ClientRequest) {
+        const { path, method, host, protocol } = handle._httpMessage
+        label += ` ClientRequest ${method} ${protocol}//${host}${path}`
       } else if (handle.localAddress) {
         const { localAddress, localPort, remoteAddress, remotePort } = handle
-        label += ` from ${localAddress}:${localPort} to ${remoteAddress}:${remotePort}`
-        // } else {
+        if (remoteAddress === undefined) {
+          label += ` local ${localAddress}:${localPort}`
+        } else {
+          label += ` local ${localAddress}:${localPort} remote ${remoteAddress}:${remotePort}`
+        }
+      } else if (handle._handle) {
+        const underlyingHandle = handle._handle
+        const underlyingClassName = underlyingHandle && underlyingHandle.constructor && underlyingHandle.constructor.name
+        label += ` <-> ${underlyingClassName || 'unknown'}`
+      } else {
+        label += ' unknown'
       }
     } else if (className === 'WriteStream') {
-      label += ` columns: ${handle.columns} rows: ${handle.rows} fd: ${handle.fd} isTTY: ${handle.isTTY}`
+      const fd = ['stdin', 'stdout', 'stderr'][handle.fd] || `fd: ${handle.fd}`
+      label += ` ${fd} ${handle.columns}x${handle.rows} isTTY: ${handle.isTTY}`
     } else if (className === 'Server') {
       label += ` connections: ${handle._connections} events: ${handle._eventsCount}`
     } else if (className === 'ChildProcess') {
-      label += ` pid: ${handle.pid} ${handle.spawnfile}`
+      label += ` pid: ${handle.pid}`
+      if (handle.spawnargs) {
+        label += ` ${handle.spawnargs.map(value => ('' + value).replaceAll(/ /g, '␣'))}`
+      } else {
+        label += ' unknown'
+      }
     }
     return { className, label }
   }
