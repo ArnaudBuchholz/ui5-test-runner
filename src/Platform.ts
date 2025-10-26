eb88b0e6fb5b31d76fcd785483c41f2a76d52daf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-10-25 23:48:52 -0400

    refactor: centralize Node.js APIs

--- src/Platform.ts
+++ src/Platform.ts
@@ -1,7 +1,37 @@
-import { stat, access } from 'node:fs/promises';
+import { access, stat } from 'node:fs/promises';
+import { createWriteStream } from 'node:fs';
+import { join } from 'node:path';
+import { BroadcastChannel, Worker, isMainThread, threadId } from 'node:worker_threads';
+import zlib from 'zlib';
+
+const WORKER_FLAGS = ['--experimental-strip-types', '--disable-warning=ExperimentalWarning']
 
 /** This class simplifies mocking during tests */
 export class Platform {
+  static readonly cwd = process.cwd;
+  static readonly threadCpuUsage = process.threadCpuUsage;
+  static readonly memoryUsage = process.memoryUsage;
+
   static readonly stat = stat;
   static readonly access = access;
+  static readonly createWriteStream = createWriteStream;
+
+  static readonly join = join;
+
+  static readonly threadId = threadId;
+  static readonly isMainThread = isMainThread;
+  static createBroadcastChannel (name: 'logger') {
+    return new BroadcastChannel(name);
+  }
+  static createWorker (name: string, data?: unknown) {
+    const workerPath = join(__dirname, `workers/${name}.ts`);
+    const worker = new Worker(workerPath, {
+      workerData: data,
+      execArgv: WORKER_FLAGS,
+    });
+    return worker;
+  }
+
+  static readonly createGzip = zlib.createGzip;
+  static readonly Z_FULL_FLUSH = zlib.constants.Z_FULL_FLUSH;
 }
