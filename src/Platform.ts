eb31448c29689805b8269cf35ef8a607935a768e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-10-29 23:01:20 -0400

    refactor: improve pattern

--- src/Platform.ts
+++ src/Platform.ts
@@ -1,7 +1,7 @@
 import { access, stat } from 'node:fs/promises';
 import { createWriteStream } from 'node:fs';
 import { join } from 'node:path';
-import { BroadcastChannel, Worker, isMainThread, threadId } from 'node:worker_threads';
+import { BroadcastChannel, Worker, isMainThread, threadId, workerData } from 'node:worker_threads';
 import zlib from 'node:zlib';
 
 const WORKER_FLAGS = ['--experimental-strip-types', '--disable-warning=ExperimentalWarning'];
@@ -9,15 +9,9 @@ const WORKER_FLAGS = ['--experimental-strip-types', '--disable-warning=Experimen
 /** This class simplifies mocking during tests */
 export class Platform {
   static readonly pid = process.pid;
-  static cwd() {
-    return process.cwd();
-  }
-  static threadCpuUsage() {
-    return process.threadCpuUsage();
-  }
-  static memoryUsage() {
-    return process.memoryUsage();
-  }
+  static readonly cwd = process.cwd.bind(process);
+  static readonly threadCpuUsage = process.threadCpuUsage.bind(process);
+  static readonly memoryUsage = process.memoryUsage.bind(process);
 
   static readonly stat = stat;
   static readonly access = access;
@@ -27,20 +21,17 @@ export class Platform {
 
   static readonly threadId = threadId;
   static readonly isMainThread = isMainThread;
-  static createBroadcastChannel(name: 'logger') {
-    return new BroadcastChannel(name);
-  }
-  static createWorker(name: string, data?: unknown) {
+  static readonly createBroadcastChannel: (name: 'logger') => BroadcastChannel = (name) => new BroadcastChannel(name);
+  static readonly createWorker: (name: string, data?: unknown) => Worker = (name, data) => {
     const workerPath = join(__dirname, `workers/${name}.ts`);
     const worker = new Worker(workerPath, {
       workerData: data,
       execArgv: WORKER_FLAGS
     });
     return worker;
-  }
+  };
+  static readonly workerData = workerData as object;
 
-  static createGzip() {
-    return zlib.createGzip();
-  }
+  static readonly createGzip = zlib.createGzip.bind(zlib);
   static readonly Z_FULL_FLUSH = zlib.constants.Z_FULL_FLUSH;
 }
