3ff157c29feca1f77c940b1094042f94dda940d4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-10-30 21:45:15 -0400

    test: assert

---
+++ src/assert.spec.ts
@@ -0,0 +1,27 @@
+import { it, expect, vi, beforeEach } from 'vitest';
+import { assert } from './assert.js';
+import { AssertionError } from 'node:assert/strict';
+import { logger } from './logger.js';
+
+vi.spyOn(logger, 'fatal');
+
+beforeEach(() => {
+  vi.clearAllMocks();
+});
+
+it('does nothing when the condition is true', () => {
+  expect(() => assert(true)).not.toThrowError();
+});
+
+it('throws an AssertionError when the condition is false', () => {
+  expect(() => assert(false, 'test')).toThrowError(new AssertionError({ message: 'test' }));
+});
+
+it('logs the error when the condition is false as fatal', () => {
+  expect(() => assert(false, 'test')).toThrowError(new AssertionError({ message: 'test' }));
+  expect(logger.fatal).toHaveBeenCalledWith({
+    source: 'assert',
+    message: 'Assertion failed',
+    error: new AssertionError({ message: 'test' })
+  });
+});
