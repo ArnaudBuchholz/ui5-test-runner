0e6fa23176049f90c9c74fb982fa827f3a5789e7
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-11-14 08:31:08 -0500

    fix: rendering and isolate start command

--- src/workers/console.ts
+++ src/workers/console.ts
@@ -1,13 +1,11 @@
-import assert from 'node:assert/strict';
 import { Platform } from '../Platform.js';
 import type { InternalLogAttributes, LogAttributes } from '../logger.js';
-import type { Configuration } from '../configuration/Configuration.js';
 
 const log = (attributes: InternalLogAttributes & LogAttributes) => {
   const { level, timestamp, processId = 0, threadId = 0, message, data } = attributes;
   const icon = {
     debug: '🐞',
-    info: '🛈',
+    info: '  ',
     warn: '⚠️',
     error: '❌',
     fatal: '💣'
@@ -19,12 +17,7 @@ const channel = Platform.createBroadcastChannel('logger');
 channel.onmessage = (event: { data: { terminate: true } | (InternalLogAttributes & LogAttributes) }) => {
   if ('terminate' in event.data) {
     channel.close();
-  } else if (!('isReady' in event.data)) {
+  } else if ('message' in event.data) {
     log(event.data);
   }
-};
-
-export const start = (configuration: Configuration) => {
-  assert.ok(Platform.isMainThread, 'Call console.start only in main thread');
-  Platform.createWorker('console', { configuration });
 };
