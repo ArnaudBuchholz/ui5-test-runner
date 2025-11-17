60d6d1e11690bbb773d5446dbd5087ea460f52e2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-11-16 23:22:00 -0500

    fix: improve test compatibility (windows) and stability (wait for process.exit call)

--- src/defaults/junit-xml-report.spec.js
+++ src/defaults/junit-xml-report.spec.js
@@ -1,20 +1,26 @@
 const { join } = require('path')
-const { createDir } = require('../tools')
+const { allocPromise, recreateDir } = require('../tools')
 const { copyFile, readFile } = require('fs').promises
 
 describe('junit-xml-report', () => {
   jest.replaceProperty(process, 'argv', ['', '', join(__dirname, '../../tmp/junit-xml-report')])
 
   const reportDir = join(__dirname, '../../tmp/junit-xml-report')
+  const mockExit = jest.spyOn(process, 'exit')
 
   beforeEach(async () => {
-    await createDir(reportDir)
+    await recreateDir(reportDir)
     await jest.resetModules()
   })
+
   async function executeXMLReporter () {
-    await require('./junit-xml-report')
+    const { promise, resolve } = allocPromise()
+    mockExit.mockClear()
+    mockExit.mockImplementation(() => resolve())
+    require('./junit-xml-report')
+    await promise
   }
-  const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => { })
+
   it('Should add attachment to system-output for failed tests with screenshot', async () => {
     await copyFile(
       join(__dirname, '../../test/reporting/jobWithFailedTestAndScreenshot.js'),
@@ -23,9 +29,10 @@ describe('junit-xml-report', () => {
 
     await executeXMLReporter()
     const junitReport = await readFile(join(reportDir, 'junit.xml'), 'utf-8')
-    expect(junitReport).toContain('<system-out>[[ATTACHMENT|junit-xml-report/C2FfERHrn7E/da43b785.png]]</system-out>')
+    expect(junitReport).toMatch(/<system-out>\[\[ATTACHMENT\|junit-xml-report[\\/]C2FfERHrn7E[\\/]da43b785.png\]\]<\/system-out>/)
     expect(mockExit).toHaveBeenCalledWith(0)
   })
+
   it('Should not add attachment to system-output for failed test without screenshot', async () => {
     await copyFile(
       join(__dirname, '../../test/reporting/jobWithFailedTestNoScreenshot.js'),
@@ -34,7 +41,7 @@ describe('junit-xml-report', () => {
 
     await executeXMLReporter()
     const junitReport = await readFile(join(reportDir, 'junit.xml'), 'utf-8')
-    expect(junitReport).not.toContain('<system-out>[[ATTACHMENT|junit-xml-report/C2FfERHrn7E/da43b785.png]]</system-out>')
+    expect(junitReport).not.toMatch(/<system-out>\[\[ATTACHMENT\|junit-xml-report[\\/]C2FfERHrn7E[\\/]da43b785.png\]\]<\/system-out>/)
     expect(mockExit).toHaveBeenCalledWith(0)
   })
 })
