69ba51cbe6326d279e07be83d382f94c999fc91d
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-11-18 07:31:39 -0500

    refactor: avoid top level await

--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,14 @@ import { CommandLine } from './configuration/CommandLine.js';
 import { Platform } from './Platform.js';
 import { execute } from './job.js';
 
-const indexOfCli = process.argv.findIndex((value) => value.endsWith('cli.ts') || value.endsWith('cli.js'));
-const configuration = await CommandLine.buildConfigurationFrom(Platform.cwd(), process.argv.slice(indexOfCli + 1));
-await execute(configuration);
+const main = async () => {
+  const indexOfCli = process.argv.findIndex((value) => value.endsWith('cli.ts') || value.endsWith('cli.js'));
+  const configuration = await CommandLine.buildConfigurationFrom(Platform.cwd(), process.argv.slice(indexOfCli + 1));
+  await execute(configuration);
+}
+
+main()
+  .catch((reason) => {
+    console.error(reason);
+    process.exitCode = -1;
+  });
