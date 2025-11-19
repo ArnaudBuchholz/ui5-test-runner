3726fe9511c9088775d875ba885e26f5a20d29d4
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-11-19 12:32:40 -0500

    chore: improving inject

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,21 +1,35 @@
 (function () {
   'use strict';
 
-  const ui5TestRunner = {
-    status: 'pending'
-  };
+  const { top } = window;
+  if (top !== null && (window !== top || window !== window.parent)) {
+    // iframe / sub window
+  } else {
+    //main window
 
-  window['ui5-test-runner'] = ui5TestRunner;
+    const ui5TestRunner = {
+      status: 'pending'
+    };
 
-  window.addEventListener('load', function () {
-    if (typeof suite === 'function') {
-      ui5TestRunner.type = 'suite';
-    //   suite()
-    //   post('addTestPages', { type: 'suite', pages })
-    } else if (!QUnit) {
-      ui5TestRunner.type = 'unknown';
-    //   post('addTestPages', { type: 'none' })
-    }
-  });
+    window['ui5-test-runner'] = ui5TestRunner;
 
-}())
+    window.addEventListener('load', function () {
+      ui5TestRunner.loaded = Date.now();
+      if (typeof suite === 'function') {
+        ui5TestRunner.type = 'suite';
+        //   suite()
+        //   post('addTestPages', { type: 'suite', pages })
+      } else if (typeof QUnit === 'undefined') {
+        ui5TestRunner.type = 'unknown';
+        //   post('addTestPages', { type: 'none' })
+      } else {
+        ui5TestRunner.type = 'QUnit';
+      }
+    });
+
+    setInterval(() => {
+      ui5TestRunner.ts = Date.now();
+    }, 250);
+  }
+
+}());
