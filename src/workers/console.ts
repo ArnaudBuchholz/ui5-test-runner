7ddc3c93864db9a6b06a263e4bd620fb805a1bb3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-11-25 23:42:43 -0500

    refactor: generate both output

--- src/workers/console.ts
+++ src/workers/console.ts
@@ -1,9 +1,26 @@
 import { Platform } from '../Platform.js';
 import type { InternalLogAttributes, LogAttributes } from '../logger.js';
 import { LogSource } from '../logger.js';
+import type { Configuration } from '../configuration/Configuration.js';
 
-const start = Date.now();
-const maxLogSourceSize = Math.max(...Object.values(LogSource).map((source) => source.length));
+const STARTED_AT = Date.now();
+const MAX_LOG_SOURCE_SIZE = Math.max(...Object.values(LogSource).map((source) => source.length));
+
+const { ci, reportDir } = (Platform.workerData as { configuration: Configuration }).configuration;
+
+const interactive = !ci && Platform.isTextTerminal;
+if (!interactive) {
+  const UTF8_BOM_CODE = '\ufeff';
+  Platform.writeOnTerminal(UTF8_BOM_CODE);
+}
+
+const out = (text: string): void => {
+  Platform.writeFileSync(Platform.join(reportDir, 'output.txt'), text, {
+    encoding: 'utf-8',
+    flag: 'a'
+  });
+  Platform.writeOnTerminal(text);
+};
 
 const formatDiff = (diffInMs: number) => {
   if (diffInMs < 0) {
@@ -23,11 +40,13 @@ const log = (attributes: InternalLogAttributes & LogAttributes) => {
     error: '❌',
     fatal: '💣'
   }[level];
-  if (source !== 'metric') {
+  if (source === 'progress') {
+
+  } else if (source !== 'metric' && level !== 'debug') {
     console.log(
       icon,
-      formatDiff(timestamp - start),
-      source.padEnd(maxLogSourceSize, ' '),
+      formatDiff(timestamp - STARTED_AT),
+      source.padEnd(MAX_LOG_SOURCE_SIZE, ' '),
       message,
       data ? JSON.stringify(data) : '',
       error ? `${(error as Error).name} ${(error as Error).message}` : ''
@@ -43,3 +62,10 @@ channel.onmessage = (event: { data: { terminate: true } | (InternalLogAttributes
     log(event.data);
   }
 };
+
+out(`       _ ____        _            _                                         
+ _   _(_) ___|      | |_ ___  ___| |_      _ __ _   _ _ __  _ __   ___ _ __ 
+| | | | |___ \\ _____| __/ _ \\/ __| __|____| '__| | | | '_ \\| '_ \\ / _ \\ '__|
+| |_| | |___) |_____| ||  __/\\__ \\ ||_____| |  | |_| | | | | | | |  __/ |   
+ \\__,_|_|____/       \\__\\___||___/\\__|    |_|   \\__,_|_| |_|_| |_|\\___|_|   \n`);
+
