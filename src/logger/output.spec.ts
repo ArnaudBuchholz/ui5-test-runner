f8a809725424cc72cc0f82f7720ff32ec5fbcc96
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-12-05 23:07:30 -0500

    refactor: new approach for worker threads

--- src/logger/output.spec.ts
+++ src/logger/output.spec.ts
@@ -3,17 +3,20 @@ import { Platform } from '../Platform.js';
 import type { LogMessage } from './types.js';
 import { LogLevel } from './types.js';
 import { LoggerOutputFactory } from './output/factory.js';
-import { LoggerOutputRenderer } from './output/LoggerOutputRenderer.js';
+import { AbstractLoggerOutput } from './output/AbstractLoggerOutput.js';
+import { workerMain } from './output.js';
+import type { Configuration } from '../configuration/Configuration.js';
 
-const appendToLoggerOutput = vi.fn();
-const closeLoggerOutput = vi.fn();
-vi.spyOn(LoggerOutputFactory, 'build').mockReturnValue({
-  appendToLoggerOutput,
-  closeLoggerOutput
-});
-const render = vi.spyOn(LoggerOutputRenderer, 'render').mockReturnValue('log');
+class TestLoggerOutput extends AbstractLoggerOutput {
+  addTextToLoggerOutput() {}
+}
 
-vi.useFakeTimers();
+const addTextToLoggerOutput = vi.spyOn(TestLoggerOutput.prototype, 'addTextToLoggerOutput').mockReturnValue();
+const addAttributesToLoggerOutput = vi
+  .spyOn(TestLoggerOutput.prototype, 'addAttributesToLoggerOutput')
+  .mockReturnValue();
+const closeLoggerOutput = vi.spyOn(TestLoggerOutput.prototype, 'closeLoggerOutput').mockReturnValue();
+vi.spyOn(LoggerOutputFactory, 'build').mockImplementation((configuration) => new TestLoggerOutput(configuration));
 
 vi.mock('../Platform.js', () => {
   const channel = {
@@ -22,73 +25,48 @@ vi.mock('../Platform.js', () => {
     close: vi.fn()
   };
   const Platform = {
-    workerData: { configuration: { cwd: './tmp', reportDir: './tmp' } },
     createBroadcastChannel: vi.fn(() => channel)
   };
   return { Platform };
 });
 
-beforeEach(() => vi.clearAllMocks());
+beforeEach(() => {
+  vi.clearAllMocks();
+  workerMain({ configuration: { cwd: './tmp', reportDir: './tmp' } as Configuration });
+});
 
 const postMessage = (channel: ReturnType<typeof Platform.createBroadcastChannel>, data: LogMessage) =>
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument
   channel.onmessage({ data } as any);
 
-it('broadcasts an initial { ready: true }', async () => {
-  await import('./output.js');
+it('broadcasts an initial { ready: true }', () => {
   const channel = Platform.createBroadcastChannel('logger');
   expect(channel.postMessage).toHaveBeenCalledWith({ command: 'ready', source: 'output' } satisfies LogMessage);
 });
 
-it('closes the broadcast channel and the loggerOutput when the terminate signal is received', async () => {
-  await import('./output.js');
+it('displays an initial message', () => {
+  expect(addTextToLoggerOutput).toHaveBeenCalled();
+});
+
+it('forwards log attributes to the loggerOutput', () => {
+  const channel = Platform.createBroadcastChannel('logger');
+  const logMessage: LogMessage = {
+    command: 'log',
+    timestamp: 123,
+    level: LogLevel.info,
+    processId: 1,
+    threadId: 2,
+    isMainThread: true,
+    source: 'job',
+    message: 'Hello World !'
+  };
+  postMessage(channel, logMessage);
+  expect(addAttributesToLoggerOutput).toHaveBeenCalledWith(logMessage);
+});
+
+it('closes the broadcast channel and the loggerOutput when the terminate signal is received', () => {
   const channel = Platform.createBroadcastChannel('logger');
   postMessage(channel, { command: 'terminate' });
   expect(channel.close).toHaveBeenCalled();
   expect(closeLoggerOutput).toHaveBeenCalled();
-});
-
-it('logs traces if renderer allows it', async () => {
-  await import('./output.js');
-  const channel = Platform.createBroadcastChannel('logger');
-  const logMessage: LogMessage = {
-    command: 'log',
-    timestamp: 123,
-    level: LogLevel.info,
-    processId: 1,
-    threadId: 2,
-    isMainThread: true,
-    source: 'job',
-    message: 'Hello World !'
-  };
-  postMessage(channel, logMessage);
-  expect(render).toHaveBeenCalledWith(
-    Platform.workerData.configuration,
-    vi.getMockedSystemTime()?.getTime(),
-    logMessage
-  );
-  expect(appendToLoggerOutput).toHaveBeenCalledWith('log');
-});
-
-it('does not log traces if renderer denies it', async () => {
-  await import('./output.js');
-  const channel = Platform.createBroadcastChannel('logger');
-  const logMessage: LogMessage = {
-    command: 'log',
-    timestamp: 123,
-    level: LogLevel.info,
-    processId: 1,
-    threadId: 2,
-    isMainThread: true,
-    source: 'job',
-    message: 'Hello World !'
-  };
-  render.mockReturnValueOnce();
-  postMessage(channel, logMessage);
-  expect(render).toHaveBeenCalledWith(
-    Platform.workerData.configuration,
-    vi.getMockedSystemTime()?.getTime(),
-    logMessage
-  );
-  expect(appendToLoggerOutput).not.toHaveBeenCalledWith('log');
 });
--- src/logger/output.ts
+++ src/logger/output.ts
@@ -1,40 +1,32 @@
 import { Platform } from '../Platform.js';
-import type { InternalLogAttributes, LogMessage } from './types.js';
+import type { LogMessage } from './types.js';
 import '../logger.js';
 import type { Configuration } from '../configuration/Configuration.js';
 import { LoggerOutputFactory } from './output/factory.js';
-import { LoggerOutputRenderer } from './output/LoggerOutputRenderer.js';
 
-const STARTED_AT = Date.now();
+export const workerMain = ({ configuration }: { configuration: Configuration }) => {
+  const loggerOutput = LoggerOutputFactory.build(configuration);
 
-const configuration = (Platform.workerData as { configuration: Configuration }).configuration;
-const loggerOutput = LoggerOutputFactory.build(configuration);
+  const channel = Platform.createBroadcastChannel('logger');
+  channel.onmessage = (event: { data: LogMessage }) => {
+    const { data: message } = event;
+    if (message.command === 'terminate') {
+      channel.close();
+      loggerOutput.closeLoggerOutput();
+      /* istanbul ignore else */
+    } else if (message.command === 'log') {
+      loggerOutput.addAttributesToLoggerOutput(message);
+    }
+  };
 
-const log = (attributes: InternalLogAttributes) => {
-  const rendered = LoggerOutputRenderer.render(configuration, STARTED_AT, attributes);
-  if (rendered) {
-    loggerOutput.appendToLoggerOutput(rendered);
-  }
+  loggerOutput.addTextToLoggerOutput(`       _ ____        _            _                                         
+  _   _(_) ___|      | |_ ___  ___| |_      _ __ _   _ _ __  _ __   ___ _ __ 
+  | | | | |___ \\ _____| __/ _ \\/ __| __|____| '__| | | | '_ \\| '_ \\ / _ \\ '__|
+  | |_| | |___) |_____| ||  __/\\__ \\ ||_____| |  | |_| | | | | | | |  __/ |   
+  \\__,_|_|____/       \\__\\___||___/\\__|    |_|   \\__,_|_| |_|_| |_|\\___|_|   \n`);
+
+  channel.postMessage({
+    command: 'ready',
+    source: 'output'
+  } satisfies LogMessage);
 };
-
-const channel = Platform.createBroadcastChannel('logger');
-channel.onmessage = (event: { data: LogMessage }) => {
-  const { data: message } = event;
-  if (message.command === 'terminate') {
-    channel.close();
-    loggerOutput.closeLoggerOutput();
-  } else if (message.command === 'log') {
-    log(message);
-  }
-};
-
-loggerOutput.appendToLoggerOutput(`       _ ____        _            _                                         
- _   _(_) ___|      | |_ ___  ___| |_      _ __ _   _ _ __  _ __   ___ _ __ 
-| | | | |___ \\ _____| __/ _ \\/ __| __|____| '__| | | | '_ \\| '_ \\ / _ \\ '__|
-| |_| | |___) |_____| ||  __/\\__ \\ ||_____| |  | |_| | | | | | | |  __/ |   
- \\__,_|_|____/       \\__\\___||___/\\__|    |_|   \\__,_|_| |_|_| |_|\\___|_|   \n`);
-
-channel.postMessage({
-  command: 'ready',
-  source: 'output'
-} satisfies LogMessage);
