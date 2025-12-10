8219cf319385e724dfb6ccf0a884cd56dc663f60
Arnaud Buchholz <arnaud.buchholz@free.fr>
2025-12-10 13:36:43 -0500

    refactor: static logger copies report output to terminal

--- src/logger/output/StaticLoggerOutput.ts
+++ src/logger/output/StaticLoggerOutput.ts
@@ -2,7 +2,10 @@ import { Platform } from '../../Platform.js';
 import { AbstractLoggerOutput } from './AbstractLoggerOutput.js';
 
 export class StaticLoggerOutput extends AbstractLoggerOutput {
-  addTextToLoggerOutput(formatted: string, raw: string) {
-    Platform.writeOnTerminal(raw);
+  protected override addToReport(rawText: string): void {
+    super.addToReport(rawText);
+    Platform.writeOnTerminal(rawText);
   }
+
+  addTextToLoggerOutput() {}
 }
