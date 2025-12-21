c1a05e793687b33185899392b45f0d91ea4bf310
ArnaudBuchholz <arnaud.buchholz@free.fr>
2025-12-21 16:23:42 -0500

    refactor: NO_WORKERS mode

--- src/Platform.ts
+++ src/Platform.ts
@@ -97,8 +97,27 @@ export class Platform {
   static readonly createBroadcastChannel: (name: 'logger') => BroadcastChannel = (name) => new BroadcastChannel(name);
   static readonly createWorker: (name: string, data?: unknown) => Worker = (name, data) => {
     const extension = extname(import.meta.url);
-    const bootstrapPath = join(__dirname, 'workerBootstrap' + extension);
     const workerPath = './' + name + extension;
+    if (process.env['NO_WORKERS']) {
+      void (async () => {
+        const { workerMain } = (await import(workerPath)) as { workerMain: (data: unknown) => void };
+        workerMain(data);
+        if (__developmentMode) {
+          console.log(`${ANSI_BLUE}[~]${ANSI_WHITE}Fiber ${name} online`);
+        }
+      })();
+      return {
+        on: (event, callback) => {
+          if (event === 'exit') {
+            if (__developmentMode) {
+              console.log(`${ANSI_BLUE}[~]${ANSI_WHITE}Fiber ${name} offline`);
+            }
+            callback(0);
+          }
+        }
+      } as Worker;
+    }
+    const bootstrapPath = join(__dirname, 'workerBootstrap' + extension);
     const js2tsUrl = new URL('../js2ts.mjs', import.meta.url).toString();
     const execArgv = extension === '.ts' ? ['--no-warnings', '--import', js2tsUrl] : [];
     const worker = new Worker(bootstrapPath, {
