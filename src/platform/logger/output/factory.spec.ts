7a86d097938b1182403770b6cbd89fdc06715c21
Arnaud Buchholz <arnaud.buchholz@free.fr>
2026-01-19 07:27:16 -0500

    refactor: adds missing parameter

--- src/platform/logger/output/factory.spec.ts
+++ src/platform/logger/output/factory.spec.ts
@@ -1,15 +1,15 @@
 import { it, expect } from 'vitest';
 import { LoggerOutputFactory } from './factory.js';
-import type { Configuration } from '../../configuration/Configuration.js';
+import type { Configuration } from '../../../configuration/Configuration.js';
 import { StaticLoggerOutput } from './StaticLoggerOutput.js';
 import { InteractiveLoggerOutput } from './InteractiveLoggerOutput.js';
 
 it('creates an instance of StaticLoggerOutput is ci is set', () => {
-  const output = LoggerOutputFactory.build({ ci: true } as Configuration);
+  const output = LoggerOutputFactory.build({ ci: true } as Configuration, Date.now());
   expect(output).toBeInstanceOf(StaticLoggerOutput);
 });
 
 it('creates an instance of InteractiveLoggerOutput otherwise', () => {
-  const output = LoggerOutputFactory.build({} as Configuration);
+  const output = LoggerOutputFactory.build({} as Configuration, Date.now());
   expect(output).toBeInstanceOf(InteractiveLoggerOutput);
 });
