db543db19c63e28266bc80a4b4497404e8e63321
Arnaud Buchholz <arnaud.buchholz@free.fr>
2026-02-11 13:04:24 -0500

    refactor: building the report

--- src/agent/qunit.ts
+++ src/agent/qunit.ts
@@ -1,10 +1,34 @@
 import type { AgentState } from '../types/AgentState.js';
+import type { CommonTestReport } from '../types/CommonTestReportFormat.js';
+import { UI5_TEST_RUNNER } from './contants.js';
 import { state } from './state.js';
 
 type QUnitState = Extract<AgentState, { type: 'QUnit' }>;
+type QUnitTestDoneDetails = Parameters<Parameters<typeof QUnit.testDone>[0]>[0] & {
+  skipped?: true;
+  todo?: true;
+};
 
 const updateState = (updates: Partial<QUnitState>) => {
   Object.assign(state, updates);
+};
+
+const report: CommonTestReport['results'] = {
+  tool: {
+    name: 'QUnit',
+  },
+  summary: {
+    failed: 0,
+    other: 0,
+    passed: 0,
+    pending: 0,
+    skipped: 0,
+    start: 0,
+    stop: 0,
+    tests: 0,
+    duration: 0
+  },
+  tests: []
 };
 
 export const qunit = () => {
@@ -19,15 +43,50 @@ export const qunit = () => {
     })
   );
 
-  QUnit.testDone(() =>
+  QUnit.testStart((details) => {
+    report.tests.push({
+      name: details.name,
+      suite: [details.module],
+      duration: 0,
+      status: 'other'
+    });
+  });
+
+  QUnit.testDone((details: QUnitTestDoneDetails) => {
+    if (!report.tests.length ) {
+      // TODO: find a way to log the issue
+      return;
+    }
+    const test = report.tests[report.tests.length - 1]!; // length has been tested
+    // TODO: check that the test correspond to the one added on testStart
+    test.duration = details.runtime;
+    ++report.summary.tests;
+    if (details.failed > 0) {
+      test.status = 'failed';
+      test.message = ''; // Need to capture the first failed assertion
+      ++report.summary.failed;
+    } else if (details.passed > 0) {
+      test.status = 'passed';
+      ++report.summary.passed;
+    } else if (details.skipped) {
+      test.status = 'skipped';
+      ++report.summary.skipped;
+    } else if (details.todo) {
+      test.status = 'pending';
+      ++report.summary.pending;
+    } else {
+      test.status = 'other';
+      ++report.summary.other;
+    }
     updateState({
       executed: ++executed
     })
-  );
+  });
 
-  QUnit.done(() =>
+  QUnit.done(() => {
+    window[UI5_TEST_RUNNER].report = report;
     updateState({
       done: true
-    })
-  );
+    });
+  });
 };
