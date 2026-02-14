63eb884b187f803d862fbb7eab7453344fa5edfc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-02-13 23:25:18 -0500

    refactor: memoize result as it may not change

--- src/platform/version.ts
+++ src/platform/version.ts
@@ -1,10 +1,11 @@
 import { FileSystem } from './FileSystem.js';
 import { __sourcesRoot } from './constants.js';
 import { Path } from './Path.js';
+import { memoize } from '../utils/memoize.js';
 
-export const version = async (): Promise<string> => {
+export const version = memoize(async (): Promise<string> => {
   const { name, version } = JSON.parse(
     await FileSystem.readFile(Path.join(__sourcesRoot, '../package.json'), 'utf8')
   ) as { name: string; version: string };
   return `${name}@${version}`;
-};
+});
