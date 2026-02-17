3c233a934b038ae69f7c1ae4592df4ccda7635db
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-02-16 23:19:51 -0500

    refactor: report merger

---
+++ src/modes/test/report.spec.ts
@@ -0,0 +1,126 @@
+import { it, expect, beforeEach } from 'vitest';
+import { TestReportMerger } from './report.js';
+import type { CommonTestReport } from '../../types/CommonTestReportFormat.js';
+import { createEmptyTestResults } from '../../types/CommonTestReportFormat.js';
+
+const comparableTestReport = (testResults: Partial<CommonTestReport['results']> = {}) => {
+  const emptyTestReport: CommonTestReport = {
+    reportFormat: 'CTRF',
+    specVersion: 'pre-1.0',
+    reportId: expect.any(String) as string,
+    timestamp: expect.any(String) as string,
+    generatedBy: 'ui5-test-runner@1.2.3',
+    results: createEmptyTestResults()
+  };
+  Object.assign(emptyTestReport.results, testResults);
+  emptyTestReport.results.summary.start = expect.any(Number) as number;
+  emptyTestReport.results.summary.stop = expect.any(Number) as number;
+  emptyTestReport.results.summary.duration = expect.any(Number) as number;
+  return emptyTestReport;
+};
+
+let report: TestReportMerger;
+
+beforeEach(async () => {
+  report = new TestReportMerger();
+  await report.initialize();
+});
+
+it('returns an empty report', () => {
+  expect(report.merged).toStrictEqual(comparableTestReport());
+});
+
+it('merges test results (1)', () => {
+  const test0: CommonTestReport['results']['tests'][number] = {
+    name: 'test0',
+    status: 'passed',
+    duration: 1
+  };
+  report.merge({
+    tool: { name: 'test' },
+    tests: [test0],
+    summary: {
+      failed: 0,
+      other: 0,
+      passed: 1,
+      pending: 0,
+      skipped: 0,
+      start: 123,
+      stop: 456,
+      tests: 1
+    }
+  });
+  expect(report.merged).toStrictEqual(
+    comparableTestReport({
+      tool: { name: 'test' },
+      tests: [test0],
+      summary: {
+        failed: 0,
+        other: 0,
+        passed: 1,
+        pending: 0,
+        skipped: 0,
+        start: 0,
+        stop: 0,
+        tests: 1
+      }
+    })
+  );
+});
+
+it('merges test results (2)', () => {
+  const test0: CommonTestReport['results']['tests'][number] = {
+    name: 'test0',
+    status: 'passed',
+    duration: 1
+  };
+  report.merge({
+    tool: { name: 'test' },
+    tests: [test0],
+    summary: {
+      failed: 0,
+      other: 0,
+      passed: 1,
+      pending: 0,
+      skipped: 0,
+      start: 123,
+      stop: 456,
+      tests: 1
+    }
+  });
+  const test1: CommonTestReport['results']['tests'][number] = {
+    name: 'test1',
+    status: 'failed',
+    duration: 1
+  };
+  report.merge({
+    tool: { name: 'test' },
+    tests: [test1],
+    summary: {
+      failed: 1,
+      other: 0,
+      passed: 0,
+      pending: 0,
+      skipped: 0,
+      start: 123,
+      stop: 456,
+      tests: 1
+    }
+  });
+  expect(report.merged).toStrictEqual(
+    comparableTestReport({
+      tool: { name: 'test' },
+      tests: [test0, test1],
+      summary: {
+        failed: 1,
+        other: 0,
+        passed: 1,
+        pending: 0,
+        skipped: 0,
+        start: 0,
+        stop: 0,
+        tests: 2
+      }
+    })
+  );
+});
--- src/modes/test/report.ts
+++ src/modes/test/report.ts
@@ -1,18 +1,19 @@
 import { assert } from '../../platform/assert.js';
 import { version } from '../../platform/version.js';
-import { createEmptyTestResults, type CommonTestReport } from '../../types/CommonTestReportFormat.js';
+import { createEmptyTestResults } from '../../types/CommonTestReportFormat.js';
+import type { CommonTestReport } from '../../types/CommonTestReportFormat.js';
 import { randomUUID } from 'node:crypto';
 
-export class TestReportGenerator {
-  private _report: CommonTestReport | undefined;
+export class TestReportMerger {
+  private _merged: CommonTestReport | undefined;
 
-  get report (): CommonTestReport {
-    assert(this._report !== undefined);
-    return this._report;
+  get merged(): CommonTestReport {
+    assert(this._merged !== undefined);
+    return this._merged;
   }
 
   async initialize(): Promise<void> {
-    this._report = {
+    this._merged = {
       reportFormat: 'CTRF',
       specVersion: 'pre-1.0',
       reportId: randomUUID(),
@@ -20,13 +21,19 @@ export class TestReportGenerator {
       generatedBy: await version(),
       results: createEmptyTestResults()
     };
-    this._report.results.summary.start = Date.now();
+    this._merged.results.summary.start = Date.now();
   }
 
-  merge (testResults: CommonTestReport['results']) {
-    assert(this._report !== undefined);
-    // TODO: what about testResults.tool
-    const { results } = this._report;
+  merge(testResults: CommonTestReport['results']) {
+    assert(this._merged !== undefined);
+    const { name: toolName } = this._merged.results.tool;
+    if (toolName === '') {
+      this._merged.results.tool = testResults.tool;
+    } else {
+      assert(toolName === testResults.tool.name); // TODO: what to do otherwise ?
+    }
+    const { results } = this._merged;
+    // eslint-disable-next-line unicorn/prefer-spread -- because not scalable
     results.tests = results.tests.concat(testResults.tests);
     const summaryFields = ['tests', 'passed', 'failed', 'skipped', 'pending', 'other'] as const;
     for (const summaryField of summaryFields) {
@@ -34,12 +41,12 @@ export class TestReportGenerator {
     }
   }
 
-  finalize () {
-    assert(this._report !== undefined);
-    const { summary } = this._report.results;
+  finalize() {
+    assert(this._merged !== undefined);
+    const { summary } = this._merged.results;
     summary.stop = Date.now();
     summary.duration = summary.start - summary.stop;
   }
 }
 
-export const report = new TestReportGenerator();
+export const report = new TestReportMerger();
