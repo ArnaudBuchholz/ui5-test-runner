692769ae5e8f90a611477640d0cbaed64cfae9c9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-03-09 22:42:21 -0400

    refactor: linter fixes

--- src/reports/html.ts
+++ src/reports/html.ts
@@ -1,13 +1,15 @@
-import { __developmentMode, __sourcesRoot, FileSystem, Path } from "../platform/index.js";
-import type { Configuration } from "../configuration/Configuration.js";
-import type { CommonTestReport } from "../types/CommonTestReportFormat.js";
+import { __developmentMode, __sourcesRoot, FileSystem, Path } from '../platform/index.js';
+import type { Configuration } from '../configuration/Configuration.js';
+import type { CommonTestReport } from '../types/CommonTestReportFormat.js';
 
 export const generateHtmlReport = async (configuration: Configuration, report: CommonTestReport) => {
   const path = __developmentMode
     ? Path.join(__sourcesRoot, '../dist', 'ui5-test-runner-html-report.js')
     : Path.join(__sourcesRoot, 'ui5-test-runner-html-report.js');
   const htmlReportJs = await FileSystem.readFile(path, 'utf8');
-  FileSystem.writeFileSync(Path.join(configuration.reportDir, 'report.html'), `<!DOCTYPE html>
+  FileSystem.writeFileSync(
+    Path.join(configuration.reportDir, 'report.html'),
+    `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
@@ -20,6 +22,7 @@ export const generateHtmlReport = async (configuration: Configuration, report: C
     <script type="module">${htmlReportJs}</script>
   </body>
 </html>
-`, 'utf8')
-
-}
+`,
+    'utf8'
+  );
+};
