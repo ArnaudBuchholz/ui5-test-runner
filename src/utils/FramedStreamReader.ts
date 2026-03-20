deeefb21ba9d23404de20460f03f89c55d3e76c7
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-03-20 11:55:23 -0400

    refactor: reduce complexity

--- src/utils/FramedStreamReader.ts
+++ src/utils/FramedStreamReader.ts
@@ -1,10 +1,23 @@
-import assert from 'node:assert';
-import { Exit, FileSystem } from '../platform/index.js';
+import type { IRegisteredAsyncTask } from '../platform/index.js';
+import { Exit, FileSystem, assert } from '../platform/index.js';
 import { setTimeout } from 'node:timers/promises';
 
 export interface IFramedStreamReader {
   read(): AsyncIterableIterator<Buffer>;
 }
+
+type FrameExtractorItem =
+  | {
+      type: 'frame';
+      frame: Buffer;
+    }
+  | {
+      type: 'incomplete';
+      remaining: Buffer;
+    }
+  | {
+      type: 'end';
+    };
 
 export class FramedStreamReader {
   static create(fileName: string, pollIntervalMs = 500): IFramedStreamReader {
@@ -19,45 +32,74 @@ export class FramedStreamReader {
     this._pollIntervalMs = pollIntervalMs;
   }
 
-  async *read(): AsyncIterableIterator<Buffer> {
-    let startPos = 0;
-    let reading = true;
-    let buffer = Buffer.alloc(0);
+  private *_extractFrames(buffer: Buffer): Generator<FrameExtractorItem> {
+    let workingBuffer = buffer;
+    while (workingBuffer.length >= 4) {
+      const size = workingBuffer.readUInt32BE();
+      if (size === 0) {
+        yield { type: 'end' };
+        return;
+      }
+      if (workingBuffer.length < size + 4) {
+        yield { type: 'incomplete', remaining: workingBuffer };
+        return;
+      }
+      yield {
+        type: 'frame',
+        frame: workingBuffer.subarray(4, size + 4)
+      };
+      workingBuffer = Buffer.from(workingBuffer.subarray(size + 4));
+    }
+    yield { type: 'incomplete', remaining: workingBuffer };
+  }
 
-    const task = Exit.registerAsyncTask({
+  private _startPos = 0;
+  private _reading = true;
+  private _buffer: Buffer = Buffer.alloc(0);
+  private _task: IRegisteredAsyncTask | undefined;
+
+  async *_read(end: number) {
+    const fileStream = FileSystem.createReadStream(this._fileName, {
+      start: this._startPos,
+      end,
+      highWaterMark: 64 * 1024
+    });
+    for await (const chunk of fileStream) {
+      assert(chunk instanceof Buffer);
+      this._buffer = Buffer.concat([this._buffer, chunk]);
+      this._startPos += chunk.length;
+      for (const result of this._extractFrames(this._buffer)) {
+        if (result.type === 'end') {
+          this._reading = false;
+          this._task?.unregister();
+          return;
+        }
+        if (result.type === 'frame') {
+          yield result.frame;
+        } else {
+          this._buffer = result.remaining;
+        }
+      }
+    }
+  }
+
+  async *read(): AsyncIterableIterator<Buffer> {
+    assert(this._task === undefined, 'read is already in progress');
+    this._task = Exit.registerAsyncTask({
       name: `FramedStreamReader(${this._fileName})`,
       stop: () => {
-        reading = false;
+        this._reading = false;
       }
     });
 
-    while (reading) {
+    while (this._reading) {
       const stats = await FileSystem.stat(this._fileName);
-      if (stats.size > startPos) {
-        const fileStream = FileSystem.createReadStream(this._fileName, {
-          start: startPos,
-          end: stats.size - 1,
-          highWaterMark: 64 * 1024
-        });
-        for await (const chunk of fileStream) {
-          assert.ok(chunk instanceof Buffer);
-          buffer = Buffer.concat([buffer, chunk]);
-          startPos += chunk.length;
-          while (buffer.length >= 4) {
-            const size = buffer.readUInt32BE();
-            if (size === 0) {
-              task.unregister();
-              return;
-            }
-            if (buffer.length < size + 4) {
-              break;
-            }
-            yield buffer.subarray(4, size + 4);
-            buffer = Buffer.from(buffer.subarray(size + 4));
-          }
-        }
+      if (stats.size > this._startPos) {
+        yield* this._read(stats.size - 1);
       }
-      await setTimeout(this._pollIntervalMs);
+      if (this._reading) {
+        await setTimeout(this._pollIntervalMs);
+      }
     }
   }
 }
