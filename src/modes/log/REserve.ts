7169f36aebf2a4c2eab2bcdf88c94723779484f3
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-03-24 15:50:41 -0400

    refactor: simplify configuration

--- src/modes/log/REserve.ts
+++ src/modes/log/REserve.ts
@@ -1,21 +1,22 @@
 import type { Configuration } from 'reserve';
 import type { ILogStorage } from './ILogStorage.js';
 
-export const buildConfiguration = (storage: ILogStorage): Configuration => ({
+const getAsInt = (parameters: URLSearchParams, key: string): number | undefined => {
+  const value = parameters.get(key);
+  return value ? Number.parseInt(value) : undefined;
+};
+
+export const buildREserveConfiguration = (storage: ILogStorage): Configuration => ({
   mappings: [
     {
       method: 'GET',
       match: 'query',
       custom: ({ url }) => {
         const searchParameters = new URLSearchParams(new URL(url!, 'http://localhost').search);
-        const fromParameter = searchParameters.get('from');
-        const from = fromParameter ? Number.parseInt(fromParameter) : undefined;
-        const toParameter = searchParameters.get('to');
-        const to = toParameter ? Number.parseInt(toParameter) : undefined;
-        const skipParameter = searchParameters.get('skip');
-        const skip = skipParameter ? Number.parseInt(skipParameter) : undefined;
-        const limitParameter = searchParameters.get('limit');
-        const limit = limitParameter ? Number.parseInt(limitParameter) : undefined;
+        const from = getAsInt(searchParameters, 'from');
+        const to = getAsInt(searchParameters, 'to');
+        const skip = getAsInt(searchParameters, 'skip');
+        const limit = getAsInt(searchParameters, 'limit');
         const filter = searchParameters.get('filter') ?? undefined;
         return [storage.fetch({ from, to, skip, limit, filter })];
       }
