b55128c546a999b0c67827fb6ee8b88880b2ddbc
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-04-02 09:40:38 -0400

    refactor: provides an abstrace base class

--- src/types/UserInterfaceController.ts
+++ src/types/UserInterfaceController.ts
@@ -10,7 +10,7 @@ export type UIEvent<State, Actions> = Partial<Writable<State>> & {
   action?: Actions;
 };
 
-export type IUIController<Settings, State, Actions> = {
+export type IUIController<Settings extends object, State extends object, Actions extends string> = {
   /**
    * Connect the UI to the controller.
    * Everytime the control requires an update in the UI, the update callback is called with state values that changed
@@ -24,3 +24,48 @@ export type IUIController<Settings, State, Actions> = {
    */
   interaction(event: UIEvent<State, Actions>): void;
 };
+
+export abstract class AbstractUIController<
+  Settings extends object,
+  State extends object,
+  Actions extends string
+> implements IUIController<Settings, State, Actions> {
+  protected _state = {} as State;
+  protected _settings = {} as Settings;
+  protected _updateCb: (event: Partial<State>) => void = () => {};
+
+  protected _assign({ ...stateDiff }: Partial<State>): Partial<State> {
+    for (const key of Object.keys(stateDiff) as (keyof State)[]) {
+      if (stateDiff[key] === this._state[key]) {
+        delete stateDiff[key];
+      }
+    }
+    Object.assign(this._state, stateDiff);
+    return stateDiff;
+  }
+
+  protected _update(stateDiff: Partial<State>): Partial<State> {
+    stateDiff = this._assign(stateDiff);
+    this._updateCb({ ...stateDiff });
+    return stateDiff;
+  }
+
+  protected _onConnect(): void {}
+
+  connect(update: (event: Partial<State>) => void) {
+    this._updateCb = update;
+    this._onConnect();
+    return {
+      initialState: { ...this._state },
+      settings: this._settings
+    };
+  }
+
+  protected abstract _onInteraction(stateDiff: Partial<State>, action?: Actions): void;
+
+  interaction(event: UIEvent<State, Actions>) {
+    const { action, ...state } = event;
+    const stateDiff = this._assign(state as Partial<State>);
+    this._onInteraction(stateDiff, action);
+  }
+}
