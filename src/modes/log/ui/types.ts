5abcdc9d09780503ae60b15a0e8f6ea6ebd74607
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-04-05 00:05:19 -0400

    doc: improve filter documentation

--- src/modes/log/ui/types.ts
+++ src/modes/log/ui/types.ts
@@ -14,6 +14,14 @@ export type State = {
   absoluteTimerangeTo: number; /* EPOCH */
   autorefresh: boolean;
   autorefreshInterval: (typeof AUTO_REFRESH_SETTINGS)[number]['key'];
+  /**
+   * - Almost any JavaScript expression is supported (some operators like the coalesce and the assignment ones missing)
+   * - The `level` member is converted to string to make it more readable, based on the `LogLevel` type described in `src/platform/logger/types.ts`
+   * - The `data` member may contain anything, yet when a filter is created with a non-existing member, it does not fail the expression. For instance, `data.unknown.not_a_member === undefined` returns `true` and does not fail if `data` or `unknown` do not exist.
+   * - Examples:
+   *   - `processId === 16616`
+   *   - `level === "info" && source === "job" && (message.includes("page") || message.includes("test"))`
+   */
   filter: string;
   readonly errorMessage: string;
   readonly logs: InternalLogAttributes[];
