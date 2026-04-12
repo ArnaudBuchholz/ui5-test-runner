9d69213318546936cdb8e8dbba8b4e54f87aa20f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-04-12 00:19:44 -0400

    refactor: improve task handling

--- src/browsers/puppeteer.ts
+++ src/browsers/puppeteer.ts
@@ -1,53 +1,67 @@
 import { logger, Exit, Process } from '../platform/index.js';
-import type { IBrowser } from './IBrowser.js';
+import type { BrowserCapabilities, BrowserSettings, IBrowser } from './IBrowser.js';
 import type { launch as launchFunction, Browser } from 'puppeteer';
 import { Npm } from '../Npm.js';
 
 export const factory = async (): Promise<IBrowser> => {
   const puppeteer = await Npm.import('puppeteer');
-  // TODO: may need to npx puppeteer browsers install chrome
   const { launch } = puppeteer as { launch: typeof launchFunction };
   let browser: Browser | undefined;
-  let task: ReturnType<typeof Exit.registerAsyncTask>;
+  const abortController = new AbortController();
+  const { signal } = abortController;
+  const task = Exit.registerAsyncTask({
+    name: 'puppeteer',
+    async stop() {
+      abortController.abort();
+      await browser?.close();
+    }
+  });
+
+  const launchAndInstallIfNeeded = async (settings: BrowserSettings): Promise<BrowserCapabilities> => {
+    try {
+      browser = await launch({
+        headless: !settings.visible,
+        defaultViewport: null,
+        handleSIGINT: false,
+        signal
+      });
+    } catch (error) {
+      if (error instanceof Error && error.message.startsWith('Could not find Chrome')) {
+        logger.info({
+          source: 'progress',
+          message: 'Installing chrome (puppeteer)',
+          data: { uid: '', value: 0, max: 0 }
+        });
+        await Process.spawn('npx', 'puppeteer browsers install chrome'.split(' '), {
+          shell: true,
+          signal
+        }).closed;
+        browser = await launch({
+          headless: false,
+          defaultViewport: null,
+          handleSIGINT: false,
+          signal
+        });
+      } else {
+        throw error;
+      }
+    }
+    logger.debug({ source: 'puppeteer', message: 'setup completed' });
+    return {
+      screenshotFormat: '.png'
+    };
+  };
 
   return {
     async setup(settings) {
       logger.debug({ source: 'puppeteer', message: 'setup', data: settings });
       try {
-        browser = await launch({
-          headless: !settings.visible,
-          defaultViewport: null,
-          handleSIGINT: false
-        });
+        return await launchAndInstallIfNeeded(settings);
       } catch (error) {
-        if (error instanceof Error && error.message.startsWith('Could not find Chrome')) {
-          logger.info({
-            source: 'progress',
-            message: 'Installing chrome (puppeteer)',
-            data: { uid: '', value: 0, max: 0 }
-          });
-          await Process.spawn('npx', 'puppeteer browsers install chrome'.split(' '), {
-            shell: true
-          }).closed;
-          browser = await launch({
-            headless: false,
-            defaultViewport: null,
-            handleSIGINT: false
-          });
-        } else {
-          throw error;
-        }
+        logger.error({ source: 'puppeteer', message: 'setup failed', error });
+        task[Symbol.dispose]();
+        throw error;
       }
-      logger.debug({ source: 'puppeteer', message: 'setup completed' });
-      task = Exit.registerAsyncTask({
-        name: 'puppeteer',
-        async stop() {
-          await browser?.close();
-        }
-      });
-      return {
-        screenshotFormat: '.png'
-      };
     },
 
     async newWindow(settings) {
@@ -66,16 +80,24 @@ export const factory = async (): Promise<IBrowser> => {
           throw new Error('Not implemented');
         },
         async close() {
-          await page?.close();
+          try {
+            await page?.close();
+          } catch (error) {
+            logger.error({ source: 'puppeteer', message: 'page.close failed', error });
+          }
         }
       };
     },
 
     async shutdown() {
       logger.debug({ source: 'puppeteer', message: 'shutdown' });
+      try {
+        await browser?.close();
+      } catch (error) {
+        logger.error({ source: 'puppeteer', message: 'browser.close failed', error });
+      }
       // TODO close any remaining pages
-      await browser?.close();
-      task.unregister();
+      task[Symbol.dispose]();
     }
   };
 };
