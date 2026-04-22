5986fccccbbdde2d56b74b74045a66b68022e53c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-04-21 23:59:24 -0400

    refactor: improve coverage

--- src/platform/logger/output/InteractiveLoggerOutput.spec.ts
+++ src/platform/logger/output/InteractiveLoggerOutput.spec.ts
@@ -30,7 +30,7 @@ describe('cursor management', () => {
   });
 });
 
-describe('output handling', () => {
+describe('output handling (complete scenario with errors)', () => {
   let loggerOutput: InteractiveLoggerOutput;
 
   beforeAll(() => {
@@ -92,19 +92,19 @@ describe('output handling', () => {
         errors: 0
       } satisfies PageProgressData
     } as InternalLogAttributes);
-    // loggerOutput.addAttributesToLoggerOutput({
-    //   timestamp: Date.now(),
-    //   source: 'progress',
-    //   level: LogLevel.info,
-    //   message: 'Test suite',
-    //   data: {
-    //     uid: 'page4',
-    //     max: 1,
-    //     value: 0,
-    //     type: 'unknown',
-    //     errors: 0
-    //   } satisfies PageProgressData
-    // } as InternalLogAttributes);
+    loggerOutput.addAttributesToLoggerOutput({
+      timestamp: Date.now(),
+      source: 'progress',
+      level: LogLevel.info,
+      message: 'Test suite',
+      data: {
+        uid: 'page4',
+        max: 0,
+        value: 1,
+        type: 'unknown',
+        errors: 0
+      } satisfies PageProgressData
+    } as InternalLogAttributes);
   });
 
   afterAll(() => {
@@ -131,7 +131,7 @@ describe('output handling', () => {
     it('renders main progress (with animated tick)', () => {
       expect(Terminal.write).toHaveBeenCalled();
       const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
-      expect(stripVTControlCharacters(text)).toMatch(/\[.\]\[#####-----\] 50% main\b/);
+      expect(stripVTControlCharacters(text)).toMatch(/\[.\]\[#####-----\] 50% main \(Tests: 107\+3\/300\)/);
     });
 
     it('renders page 1 progress', () => {
@@ -154,14 +154,14 @@ describe('output handling', () => {
       expect(text).toContain(`${Terminal.GREEN}QNT${Terminal.WHITE}[#####-----] 50% QUnit page with no error\n`);
     });
 
-    // it('renders page 4 progress', () => {
-    //   expect(Terminal.write).toHaveBeenCalled();
-    //   const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
-    //   expect(text).toContain(`   Test suite\n`);
-    // });
+    it('renders page 4 progress', () => {
+      expect(Terminal.write).toHaveBeenCalled();
+      const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
+      expect(text).toContain(`   Test suite\n`);
+    });
 
     it('clears the progress lines on tick', () => {
-      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(4);
+      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(5);
     });
   });
 
@@ -169,7 +169,7 @@ describe('output handling', () => {
     it('clears and rerenders the progress lines on resize (shorter)', () => {
       loggerOutput.terminalResized(40);
       // Because the new terminal width is shorter, we need to clean one additional line
-      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(7);
+      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(8);
       expect(Terminal.write).toHaveBeenCalled();
       const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
       expect(text).toContain(`[##--------] 20% ...ng progress bar)\n`);
@@ -178,13 +178,65 @@ describe('output handling', () => {
 
     it('clears and rerenders the progress lines on resize (wider)', () => {
       loggerOutput.terminalResized(120);
-      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(4);
+      expect(Terminal.eraseToEnd).toHaveBeenCalledWith(5);
       expect(Terminal.write).toHaveBeenCalled();
       const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
       expect(text).toContain(
         `${Terminal.GREEN}OPA${Terminal.WHITE}[##--------] 20% OPA page with a long message that goes beyond 40 characters (including progress bar)\n`
       );
       expect(text).toContain(`${Terminal.RED}3  ${Terminal.WHITE}[####------] 40% Failed QUnit page\n`);
+    });
+  });
+});
+
+describe('output handling (scenario without errors)', () => {
+  let loggerOutput: InteractiveLoggerOutput;
+
+  beforeAll(() => {
+    loggerOutput = new InteractiveLoggerOutput(
+      {
+        reportDir: './tmp',
+        outputInterval: 250
+      } as Configuration,
+      Date.now()
+    );
+    loggerOutput.addAttributesToLoggerOutput({
+      timestamp: Date.now(),
+      source: 'progress',
+      level: LogLevel.info,
+      message: 'main',
+      data: {
+        uid: '',
+        max: 100,
+        value: 50
+      } satisfies OverallProgressData
+    } as InternalLogAttributes);
+    loggerOutput.addAttributesToLoggerOutput({
+      timestamp: Date.now(),
+      source: 'progress',
+      level: LogLevel.info,
+      message: 'OPA page',
+      data: {
+        uid: 'page1',
+        max: 100,
+        value: 20,
+        type: 'opa',
+        errors: 0
+      } satisfies PageProgressData
+    } as InternalLogAttributes);
+  });
+
+  afterAll(() => {
+    loggerOutput.closeLoggerOutput();
+  });
+
+  describe('on tick', () => {
+    beforeEach(() => vi.advanceTimersToNextTimerAsync()); // tick
+
+    it('renders main progress (with animated tick)', () => {
+      expect(Terminal.write).toHaveBeenCalled();
+      const [text] = vi.mocked(Terminal.write).mock.calls[0]!;
+      expect(stripVTControlCharacters(text)).toMatch(/\[.\]\[#####-----\] 50% main \(Tests: 20\/100\)/);
     });
   });
 });
