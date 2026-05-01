08665cc85bd32a372232873589ae309f1d821568
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-05-01 08:08:53 -0400

    refactor: improve naming convention

--- src/reports/ui/types.ts
+++ src/reports/ui/types.ts
@@ -15,12 +15,15 @@ export type Settings = {
 
 export type State = {
   report: CommonTestReport;
-  suiteId: string;
+  filterOnSuiteId: string;
   filterOnStatus: (typeof FILTER_ON_STATUS)[number]['key'];
   search: string;
   sortBy: (typeof SORT_BY)[number]['key'];
   sortAscending: boolean;
-  readonly mode: 'open' | 'display';
+  readonly mode: /** asks user to pick a file to open */
+    | 'open'
+    /** displays report */
+    | 'display';
   readonly suites: Suite[];
   readonly tests: CommonTestReport['results']['tests'];
 };
