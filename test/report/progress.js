34e9b93aadb2a487f0b0fbf144e917b1320ba3a8
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-05-21 12:49:31 +0200

    refactor: removes useless code

--- test/report/progress.js
+++
@@ -1,7 +0,0 @@
-const { getJobProgress } = require('../../src/get-job-progress')
-
-module.exports = async (request, response, pageId, testId) => {
-  delete require.cache[require.resolve('./job.js')]
-  const job = require('./job.js')
-  return getJobProgress(job, request, response, pageId, testId)
-}
