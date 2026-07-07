3ee98500035befdc6de2ab3afef0d38ccfad60ca
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-07-07 13:34:28 -0400

    refactor: executes command

--- src/modes/test/end.ts
+++ src/modes/test/end.ts
@@ -1,5 +1,6 @@
 import { logger } from '../../platform/index.js'; 
 import type { Configuration } from '../../configuration/Configuration.js';
+import { Command } from '../../Command.js';
 
 
 export const end = async (configuration: Configuration): Promise<void> => {
@@ -7,4 +8,7 @@ export const end = async (configuration: Configuration): Promise<void> => {
     return;
   }
   logger.info({ source: 'progress', message: 'Executing end command',  pageId: undefined, data: { value: 0, max: 0 } });
+  const process = await Command.execute(configuration.end, { timeout: configuration.endTimeout });
+  await process.closed;
+  // TODO update Exit.code based on process.code
 };
