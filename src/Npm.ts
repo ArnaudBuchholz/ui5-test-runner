838d5242a3af29234cf17a7b2d076af959acb5d8
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-07-07 13:28:42 -0400

    refactor: expose cli path

--- src/Npm.ts
+++ src/Npm.ts
@@ -83,35 +83,6 @@ export class Npm {
     }
   }
 
-  /** fetch the latest version info for the given module */
-  static async getLatestVersion(moduleName: string): Promise<string> {
-    try {
-      const response = await Http.get(`https://registry.npmjs.org/${moduleName}/latest`);
-      const { version } = JSON.parse(response) as { version: string };
-      return version;
-    } catch (error) {
-      throw new Error(`Unable to fetch latest version of ${moduleName} from NPM registry`, {
-        cause: error
-      });
-    }
-  }
-
-  static async checkIfLatestVersion(moduleName: string, isLocal: boolean): Promise<void> {
-    try {
-      const { local, global } = await getRoots();
-      const { version: installedVersion } = JSON.parse(
-        await FileSystem.readFile(Path.join(isLocal ? local : global, moduleName, 'package.json'), 'utf8')
-      ) as { version: string };
-      logger.info({ source: 'npm', message: `Installed version of ${moduleName} is ${installedVersion}` });
-      const latestVersion = await this.getLatestVersion(moduleName);
-      if (latestVersion !== installedVersion) {
-        logger.warn({ source: 'npm', message: `[PKGVRS] Latest version of ${moduleName} is ${latestVersion}` });
-      }
-    } catch (error) {
-      logger.error({ source: 'npm', message: 'Failed in checkIfLatestVersion', error });
-    }
-  }
-
   /** Locate the module (or install it) then import it */
   static async import(configuration: Configuration, moduleName: string): Promise<unknown> {
     logger.debug({ source: 'npm', message: `Importing module: ${moduleName}` });
@@ -187,5 +158,38 @@ export class Npm {
     }
 
     return await this.dynamicImport(moduleName);
+  }
+
+  /** fetch the latest version info for the given module */
+  static async getLatestVersion(moduleName: string): Promise<string> {
+    try {
+      const response = await Http.get(`https://registry.npmjs.org/${moduleName}/latest`);
+      const { version } = JSON.parse(response) as { version: string };
+      return version;
+    } catch (error) {
+      throw new Error(`Unable to fetch latest version of ${moduleName} from NPM registry`, {
+        cause: error
+      });
+    }
+  }
+
+  static async checkIfLatestVersion(moduleName: string, isLocal: boolean): Promise<void> {
+    try {
+      const { local, global } = await getRoots();
+      const { version: installedVersion } = JSON.parse(
+        await FileSystem.readFile(Path.join(isLocal ? local : global, moduleName, 'package.json'), 'utf8')
+      ) as { version: string };
+      logger.info({ source: 'npm', message: `Installed version of ${moduleName} is ${installedVersion}` });
+      const latestVersion = await this.getLatestVersion(moduleName);
+      if (latestVersion !== installedVersion) {
+        logger.warn({ source: 'npm', message: `[PKGVRS] Latest version of ${moduleName} is ${latestVersion}` });
+      }
+    } catch (error) {
+      logger.error({ source: 'npm', message: 'Failed in checkIfLatestVersion', error });
+    }
+  }
+
+  static async getCliPath() {
+    return await memoizedNpmCliPath();
   }
 }
