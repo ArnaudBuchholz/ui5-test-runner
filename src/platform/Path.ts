c30f3d560d2fe2766f2e3d7e6a074b6540d4c8ad
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-07-22 14:04:19 -0400

    refactor: expose extname

--- src/platform/Path.ts
+++ src/platform/Path.ts
@@ -1,8 +1,9 @@
-import { basename, join, isAbsolute, dirname } from 'node:path';
+import { basename, dirname, extname, isAbsolute, join } from 'node:path';
 
 export class Path {
   static readonly basename = basename;
   static readonly dirname = dirname;
+  static readonly extname = extname;
   static readonly isAbsolute = isAbsolute;
   static readonly join = join;
 }
