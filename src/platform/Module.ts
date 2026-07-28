5ff5a2a74461227ac427a87eb3f45d2fac00710e
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-07-28 11:31:01 -0400

    refactor: expose findPackageJSON

--- src/platform/Module.ts
+++ src/platform/Module.ts
@@ -1,5 +1,6 @@
-import { createRequire } from 'node:module';
+import { createRequire, findPackageJSON } from 'node:module';
 
 export class Module {
   static readonly createRequire = createRequire;
+  static readonly findPackageJSON = findPackageJSON;
 }
