13113910dfb2d0fc760d56d400ae018e49eb43b7
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-07-29 11:45:23 -0400

    refactor: make it generic

--- src/utils/node/anonymize.ts
+++ src/utils/node/anonymize.ts
@@ -1,10 +1,10 @@
 import { Host } from '../../platform/index.js';
 
-export function anonymize(object: object): object {
+export function anonymize<T extends object>(object: T): T {
   const home = Host.homedir();
   const json = JSON.stringify(object);
   if (json.includes(home)) {
-    return JSON.parse(json.replaceAll(home, '~')) as object;
+    return JSON.parse(json.replaceAll(home, '~')) as T;
   }
   return object;
 }
