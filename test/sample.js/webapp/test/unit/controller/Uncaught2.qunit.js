c8f1df44a66db0897bd98148573c0b50697bc478
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-08-11 23:44:55 -0400

    test: improve scenario

--- test/sample.js/webapp/test/unit/controller/Uncaught2.qunit.js
+++ test/sample.js/webapp/test/unit/controller/Uncaught2.qunit.js
@@ -3,8 +3,15 @@
 QUnit.module('QUnit Uncaught test')
 
 QUnit.test('ok', (assert) => {
+  assert.ok(true)
+})
+
+QUnit.test('not ok', (assert) => {
   const done = assert.async()
   assert.ok(true)
-  Promise.reject(new Error('Should be detected'))
-  setTimeout(done, 50)
+  // Spend some time to ensure the first test report is collected before the error is detected
+  setTimeout(() => {
+    Promise.reject(new Error('Should be detected'))
+    setTimeout(done, 500)
+  }, 500)
 })
