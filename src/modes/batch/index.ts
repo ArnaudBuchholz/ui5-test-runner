089c4b005328dc98274757d853fa33f536ee1487
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-08-22 23:39:27 -0400

    feat: document duration and number of batch items

--- src/modes/batch/index.ts
+++ src/modes/batch/index.ts
@@ -8,14 +8,14 @@ import { buildBatchReport } from './report.js';
 import { defaults } from '../../configuration/options.js';
 import { Folder } from '../../utils/node/Folder.js';
 import { saveReport } from '../../reports/saveReport.js';
+import { formatDuration } from '../../utils/shared/string.js';
 
 export const batch = async (configuration: Configuration): Promise<void> => {
+  const batchStart = Date.now();
   await Folder.create(configuration.reportDir);
   await logger.start(configuration);
   logger.debug({ source: 'job', message: 'Configuration', data: { defaults, configuration } });
-
   await logEnvironnement();
-
   logger.info({ source: 'progress', message: 'Resolving batch items', pageId: undefined, data: { value: 0, max: 0 } });
   const items = await resolve(configuration);
   if (items.length === 0) {
@@ -23,7 +23,6 @@ export const batch = async (configuration: Configuration): Promise<void> => {
     return;
   }
   logger.info({ source: 'job', message: `Found ${items.length} batch items` });
-
   const startProcess = await start(configuration);
   let failed = 0;
   logger.info({ source: 'progress', message: 'Executing batch items', pageId: undefined, data: { value: 0, max: 0 } });
@@ -33,7 +32,12 @@ export const batch = async (configuration: Configuration): Promise<void> => {
     await parallelize((item) => batchTask(configuration, item), items, {
       parallel: configuration.parallel,
       on: (event) => {
-        if ((event.type === 'completed' && event.input.statusCode !== 0) || event.type === 'failed') {
+        if (event.type === 'completed') {
+          ++completed;
+          if (event.input.statusCode !== 0) {
+            ++failed;
+          }
+        } else if (event.type === 'failed') {
           ++failed;
           ++completed;
         }
@@ -51,9 +55,11 @@ export const batch = async (configuration: Configuration): Promise<void> => {
   } finally {
     await startProcess?.kill();
   }
-
   await saveReport(configuration, await buildBatchReport(items, configuration));
-
+  logger.info({
+    source: 'job',
+    message: `Batch items completed: passed=${completed - failed} failed=${failed} count=${completed} (${formatDuration(Date.now() - batchStart)})`
+  });
   if (failed > 0) {
     Exit.code = -1;
   }
