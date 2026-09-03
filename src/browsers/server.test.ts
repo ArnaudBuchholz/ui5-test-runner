f7d06e4b2e5f9bca44fd23b69ccdb04620ed8bba
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-09-02 23:31:10 -0400

    test: server for browser tests

---
+++ src/browsers/server.test.ts
@@ -0,0 +1,23 @@
+import { serve } from 'reserve';
+import type { Server } from 'reserve';
+
+let server: Server;
+
+export async function setup () {
+  server = serve({
+    port: 0,
+    mappings: [{
+      status: 404
+    }]
+  });
+  const { promise, resolve } = Promise.withResolvers<void>();
+  server.on('ready', ({ url }) => {
+    process.env['BROWSERS_SERVER_URL'] = url;
+    resolve();
+  });
+  return promise;
+}
+
+export async function teardown () {
+  await server.close();
+}
