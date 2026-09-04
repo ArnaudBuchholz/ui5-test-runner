2c2490c40782a80693c0fa171995a23c41e16c56
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-09-04 15:38:46 -0400

    test: implement console tests

--- src/browsers/server.test.ts
+++ src/browsers/server.test.ts
@@ -1,5 +1,6 @@
 import { serve } from 'reserve';
 import type { Server } from 'reserve';
+import { agentLogPrefix } from '../types/AgentState.js';
 
 let server: Server;
 
@@ -8,13 +9,62 @@ export async function setup() {
     port: 0,
     mappings: [
       {
+        match: '/console-log.html',
+        custom: () => [
+          `<html><script>console.log('Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/console-warn.html',
+        custom: () => [
+          `<html><script>console.warn('Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/console-error.html',
+        custom: () => [
+          `<html><script>console.error('Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/console-debug.html',
+        custom: () => [
+          `<html><script>console.debug('Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/agent-log.html',
+        custom: () => [
+          `<html><script>console.debug('${agentLogPrefix}Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/agent-warn.html',
+        custom: () => [
+          `<html><script>console.warn('${agentLogPrefix}Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
+        match: '/agent-error.html',
+        custom: () => [
+          `<html><script>console.error('${agentLogPrefix}Hello World !')</script></html>`,
+          { headers: { 'content-type': 'text/html; charset=UTF-8' } }
+        ]
+      },
+      {
         status: 404
       }
     ]
   });
   const { promise, resolve } = Promise.withResolvers<void>();
-  server.on('ready', ({ url }) => {
-    process.env['BROWSERS_SERVER_URL'] = url;
+  server.on('ready', ({ port }) => {
+    process.env['BROWSERS_SERVER_URL'] = `http://localhost:${port}/`;
     resolve();
   });
   return promise;
