8b0aea525195a60b913d46bc4e77d208b36f1849
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-09-03 23:48:50 -0400

    test: puppeteer

--- src/browsers/puppeteer.spec.ts
+++ src/browsers/puppeteer.spec.ts
@@ -1,6 +1,22 @@
-import { describe } from 'vitest';
+import { describe, vi } from 'vitest';
 import { testBrowser } from './browser.test.js';
+import { Npm } from '../Npm.js';
 
-describe('generic', () => testBrowser({
-  name: 'puppeteer',
-}));
+const mockNpmImport = vi.spyOn(Npm, 'import');
+
+describe('generic', () =>
+  testBrowser({
+    name: 'puppeteer',
+    failedSetupTestCases: [
+      {
+        label: 'launch fails',
+        setup: () => {
+          mockNpmImport.mockResolvedValueOnce({
+            launch() {
+              throw new Error('Failed');
+            }
+          });
+        }
+      }
+    ]
+  }));
