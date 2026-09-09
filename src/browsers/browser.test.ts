cff0fa63c2065c8fd44d448eee69ad6933a91aeb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2026-09-08 23:15:38 -0400

    test: add more tests

--- src/browsers/browser.test.ts
+++ src/browsers/browser.test.ts
@@ -4,8 +4,10 @@ import type { Browser } from './factory.js';
 import type { IBrowser } from './IBrowser.js';
 import { defaults } from '../configuration/options.js';
 import type { Configuration } from '../configuration/Configuration.js';
-import { logger } from '../platform/index.js';
+import { __sourcesRoot, logger, Path } from '../platform/index.js';
 import { Npm } from '../Npm.js';
+// Need to import native APIs to enable testing
+import { stat } from 'node:fs/promises';
 
 const mockNpmImport = vi.spyOn(Npm, 'import');
 
@@ -123,154 +125,210 @@ export const testBrowser = ({ name, failedSetupTestCases }: TTestBrowserArgument
         describe('console', () => {
           it('captures console.log as info', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}console-log.html` });
-            expect(logger.info).toHaveBeenCalledWith({
-              source: 'browser/console',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'log' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.info).toHaveBeenCalledWith({
+                source: 'browser/console',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'log' }
+              })
+            );
           });
 
           it('captures console.warn as warn', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}console-warn.html` });
-            expect(logger.warn).toHaveBeenCalledWith({
-              source: 'browser/console',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'warn' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.warn).toHaveBeenCalledWith({
+                source: 'browser/console',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'warn' }
+              })
+            );
           });
 
           it('captures console.error as error', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}console-error.html` });
-            expect(logger.error).toHaveBeenCalledWith({
-              source: 'browser/console',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'error' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.error).toHaveBeenCalledWith({
+                source: 'browser/console',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'error' }
+              })
+            );
           });
 
           it('captures console.debug as debug', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}console-debug.html` });
-            expect(logger.debug).toHaveBeenCalledWith({
-              source: 'browser/console',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'debug' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.debug).toHaveBeenCalledWith({
+                source: 'browser/console',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'debug' }
+              })
+            );
           });
         });
 
         describe('agent', () => {
           it('captures agent logs as debug with browser/agent source', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}agent-log.html` });
-            expect(logger.debug).toHaveBeenCalledWith({
-              source: 'browser/agent',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'debug' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.debug).toHaveBeenCalledWith({
+                source: 'browser/agent',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'debug' }
+              })
+            );
           });
 
           it('captures agent warns as warn with browser/agent source', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}agent-warn.html` });
-            expect(logger.warn).toHaveBeenCalledWith({
-              source: 'browser/agent',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'warn' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.warn).toHaveBeenCalledWith({
+                source: 'browser/agent',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'warn' }
+              })
+            );
           });
 
           it('captures agent errors as error with browser/agent source', async () => {
             await browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}agent-error.html` });
-            expect(logger.error).toHaveBeenCalledWith({
-              source: 'browser/agent',
-              message: 'Hello World !',
-              pageId: 0,
-              data: { type: 'error' }
-            });
+            await vi.waitFor(() =>
+              expect(logger.error).toHaveBeenCalledWith({
+                source: 'browser/agent',
+                message: 'Hello World !',
+                pageId: 0,
+                data: { type: 'error' }
+              })
+            );
           });
         });
 
         describe('network logs', () => {
           beforeEach(() => browser.newWindow({ pageId: 0, scripts: [], url: `${BASE_URL}network.html` }));
 
-          it('2xx', () => {
-            expect(logger.info).toHaveBeenCalledWith({
-              source: 'browser/network',
-              message: `${BASE_URL}hello.js`,
-              pageId: 0,
-              data: {
-                request: {
-                  method: 'GET',
-                  headers: expect.objectContaining({
-                    'user-agent': expect.any(String) as string
-                  }) as object
-                },
-                response: {
-                  status: 200,
-                  headers: expect.objectContaining({
-                    'content-type': 'text/javascript; charset=UTF-8'
-                  }) as object
+          it('2xx', async () => {
+            await vi.waitFor(() =>
+              expect(logger.info).toHaveBeenCalledWith({
+                source: 'browser/network',
+                message: `${BASE_URL}hello.js`,
+                pageId: 0,
+                data: {
+                  request: {
+                    method: 'GET',
+                    headers: expect.objectContaining({
+                      'user-agent': expect.any(String) as string
+                    }) as object
+                  },
+                  response: {
+                    status: 200,
+                    headers: expect.objectContaining({
+                      'content-type': 'text/javascript; charset=UTF-8'
+                    }) as object
+                  }
                 }
-              }
-            });
+              })
+            );
           });
 
-          it('4xx', () => {
-            expect(logger.warn).toHaveBeenCalledWith({
-              source: 'browser/network',
-              message: `${BASE_URL}not_found.js`,
-              pageId: 0,
-              data: {
-                request: {
-                  method: 'GET',
-                  headers: expect.objectContaining({
-                    'user-agent': expect.any(String) as string
-                  }) as object
-                },
-                response: {
-                  status: 404,
-                  headers: expect.objectContaining({
-                    // Not found
-                    'content-length': '9',
-                    'content-type': 'text/plain'
-                  }) as object
+          it('4xx', async () => {
+            await vi.waitFor(() =>
+              expect(logger.warn).toHaveBeenCalledWith({
+                source: 'browser/network',
+                message: `${BASE_URL}not_found.js`,
+                pageId: 0,
+                data: {
+                  request: {
+                    method: 'GET',
+                    headers: expect.objectContaining({
+                      'user-agent': expect.any(String) as string
+                    }) as object
+                  },
+                  response: {
+                    status: 404,
+                    headers: expect.objectContaining({
+                      // Not found
+                      'content-length': '9',
+                      'content-type': 'text/plain'
+                    }) as object
+                  }
                 }
-              }
-            });
+              })
+            );
           });
 
-          it('5xx', () => {
-            expect(logger.error).toHaveBeenCalledWith({
-              source: 'browser/network',
-              message: `${BASE_URL}server_error.js`,
-              pageId: 0,
-              data: {
-                request: {
-                  method: 'GET',
-                  headers: expect.objectContaining({
-                    'user-agent': expect.any(String) as string
-                  }) as object
-                },
-                response: {
-                  status: 500,
-                  headers: expect.objectContaining({
-                    // Internal Server Error
-                    'content-length': '21',
-                    'content-type': 'text/plain'
-                  }) as object
+          it('5xx', async () => {
+            await vi.waitFor(() =>
+              expect(logger.error).toHaveBeenCalledWith({
+                source: 'browser/network',
+                message: `${BASE_URL}server_error.js`,
+                pageId: 0,
+                data: {
+                  request: {
+                    method: 'GET',
+                    headers: expect.objectContaining({
+                      'user-agent': expect.any(String) as string
+                    }) as object
+                  },
+                  response: {
+                    status: 500,
+                    headers: expect.objectContaining({
+                      // Internal Server Error
+                      'content-length': '21',
+                      'content-type': 'text/plain'
+                    }) as object
+                  }
                 }
-              }
-            });
+              })
+            );
           });
         });
 
-        it.skip('enables initialization script kept after loading the page');
-        it.skip('enables eval');
-        it.skip('enables screenshots');
+        it('enables initialization script kept after loading the page', async () => {
+          await browser.newWindow({
+            pageId: 0,
+            scripts: [`window.addEventListener('load', () => { console.log('loaded') })`],
+            url: `${BASE_URL}page.html`
+          });
+          await vi.waitFor(() =>
+            expect(logger.info).toHaveBeenCalledWith({
+              source: 'browser/console',
+              message: 'loaded',
+              pageId: 0,
+              data: { type: 'log' }
+            })
+          );
+        });
+
+        it('enables eval', async () => {
+          const window = await browser.newWindow({
+            pageId: 0,
+            scripts: [`window.addEventListener('load', () => { console.log('loaded') })`],
+            url: `${BASE_URL}page.html`
+          });
+          await vi.waitFor(async () => {
+            const value = await window.eval(`document.querySelector('h1').innerText`);
+            expect(value).toStrictEqual('Hello World !');
+          });
+        });
+
+        it('enables screenshots', async () => {
+          const window = await browser.newWindow({
+            pageId: 0,
+            scripts: [`window.addEventListener('load', () => { console.log('loaded') })`],
+            url: `${BASE_URL}page.html`
+          });
+          const path = Path.join(__sourcesRoot, `../tmp/${name}.png`);
+          await window.screenshot(path);
+          const pathStat = await stat(path);
+          expect(pathStat.size).toBeGreaterThan(0);
+        });
       });
     });
   });
