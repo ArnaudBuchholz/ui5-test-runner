b25dbc4842bbd86f703331762ab9d189e1ff140c
arnaudbuchholz <arnaud.buchholz@free.fr>
2026-09-10 11:28:14 -0400

    refactor: linter fix

--- src/platform/version.ts
+++ src/platform/version.ts
@@ -4,7 +4,8 @@ import { Path } from './Path.js';
 import { memoize } from '../utils/shared/memoize.js';
 
 export const version = memoize(async (): Promise<{ name: string; version: string }> => {
-  return JSON.parse(
-    await FileSystem.readFile(Path.join(__sourcesRoot, '../package.json'), 'utf8')
-  ) as { name: string; version: string };
+  return JSON.parse(await FileSystem.readFile(Path.join(__sourcesRoot, '../package.json'), 'utf8')) as {
+    name: string;
+    version: string;
+  };
 });
